a72abc7a79927f263182fcd4ce8171c4dd0fed69
falsandtru <falsandtru@users.noreply.github.com>
2016-06-16 17:21:36 +0900

    fix(sequence): fix Sequence.take method

--- src/lib/monad/sequence/member/instance/take.ts
+++ src/lib/monad/sequence/member/instance/take.ts
@@ -2,6 +2,13 @@ import {Sequence} from '../../core';
 
 export default class <T, S> extends Sequence<T, S> {
   public take(n: number): Sequence<T, Sequence.Iterator<T>> {
-    return this.takeWhile((_, i) => i < n);
+    return new Sequence<T, Sequence.Iterator<T>>((iter = () => this.iterate(), cons) =>
+      Sequence.Iterator.when(
+        iter(),
+        () => cons(),
+        thunk =>
+          Sequence.Thunk.index(thunk) < n
+            ? cons(Sequence.Thunk.value(thunk), Sequence.Thunk.iterator(thunk))
+            : cons()));
   }
 }
