77f220567f90e9925697e16ccf6c8d13791902f1
falsandtru <falsandtru@users.noreply.github.com>
2016-06-16 17:53:06 +0900

    feat(sequence): change Sequence.takeWhile method interface

--- src/lib/monad/sequence/member/instance/takeWhile.ts
+++ src/lib/monad/sequence/member/instance/takeWhile.ts
@@ -1,13 +1,13 @@
 import {Sequence} from '../../core';
 
 export default class <T, S> extends Sequence<T, S> {
-  public takeWhile(f: (p: T, i: number) => boolean): Sequence<T, Sequence.Iterator<T>> {
+  public takeWhile(f: (p: T) => boolean): Sequence<T, Sequence.Iterator<T>> {
     return new Sequence<T, Sequence.Iterator<T>>((iter = () => this.iterate(), cons) =>
       Sequence.Iterator.when(
         iter(),
         () => cons(),
         thunk =>
-          f(Sequence.Thunk.value(thunk), Sequence.Thunk.index(thunk))
+          f(Sequence.Thunk.value(thunk))
             ? cons(Sequence.Thunk.value(thunk), Sequence.Thunk.iterator(thunk))
             : cons()));
   }
