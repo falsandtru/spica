9fa642d16a2246a30245f998304559299b7f5bee
falsandtru <falsandtru@users.noreply.github.com>
2016-06-16 17:21:55 +0900

    fix(sequence): fix Sequence.drop method

--- src/lib/monad/sequence/member/instance/drop.ts
+++ src/lib/monad/sequence/member/instance/drop.ts
@@ -2,6 +2,13 @@ import {Sequence} from '../../core';
 
 export default class <T, S> extends Sequence<T, S> {
   public drop(n: number): Sequence<T, Sequence.Iterator<T>> {
-    return this.dropWhile((_, i) => i < n);
+    return new Sequence<T, Sequence.Iterator<T>>((iter = () => this.iterate(), cons) =>
+      Sequence.Iterator.when(
+        iter(),
+        () => cons(),
+        (thunk, recur) =>
+          Sequence.Thunk.index(thunk) < n
+            ? recur()
+            : cons(Sequence.Thunk.value(thunk), Sequence.Thunk.iterator(thunk))));
   }
 }
