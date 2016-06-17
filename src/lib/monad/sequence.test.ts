6949c12e827b31df2ce7410e9376a45e769d4715
falsandtru <falsandtru@users.noreply.github.com>
2016-06-17 18:11:38 +0900

    test(sequence): add monad laws tests of Sequence

---
+++ src/lib/monad/sequence.test.ts
@@ -0,0 +1,55 @@
+import {Sequence} from './sequence';
+
+describe('Unit: lib/monad/sequence', () => {
+  describe('Sequence', () => {
+    it('Functor law 1', () => {
+      const f = <T>(n: T) => n;
+      const x = 0;
+      const fa = Sequence.Return(x).fmap(f);
+      const fb = f(Sequence.Return(x));
+      assert.deepStrictEqual(fa.read(), fb.read());
+    });
+
+    it('Functor law 2', () => {
+      const f = (n: number) => n + 2;
+      const g = (n: number) => n * 3;
+      const x = 1;
+      const fa = Sequence.Return(x).fmap(n => g(f(n)));
+      const fb = Sequence.Return(x).fmap(f).fmap(g);
+      assert.deepStrictEqual(fa.read(), fb.read());
+    });
+
+    it('Monad law 1', () => {
+      const f = (n: number) => Sequence.Return(n + 1);
+      const x = 0;
+      const ma = Sequence.Return(x).bind(f);
+      const mb = f(x);
+      assert.deepStrictEqual(ma.read(), mb.read());
+    });
+
+    it('Monad law 2', () => {
+      const f = (n: number) => Sequence.Return(n + 1);
+      const x = 0;
+      const ma = Sequence.Return(x);
+      const mb = ma.bind(Sequence.Return);
+      assert.deepStrictEqual(ma.read(), mb.read());
+    });
+
+    it('Monad law 3', () => {
+      const m1 = Sequence.Return(1);
+      const m2 = Sequence.Return(2);
+      const m3 = Sequence.Return(4);
+      const ma = m1
+        .bind(v1 => m2.bind(v2 => Sequence.Return(v1 + v2)))
+        .bind(n => m3.bind(v3 => Sequence.Return(n + v3)));
+      const mb = m1
+        .bind(v1 => m2.bind(v2 => m3.bind(v3 =>
+          Sequence.Return(v2 + v3)))
+            .bind(n =>
+              Sequence.Return(v1 + n)));
+      assert.deepStrictEqual(ma.read(), mb.read());
+    });
+
+  });
+
+});
