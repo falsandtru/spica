22b427c38f8a4261ab473a2ca80af812d107f467
falsandtru <falsandtru@users.noreply.github.com>
2016-06-24 01:35:27 +0900

    refactor(sequence): refactor Sequence.mplus static method

--- src/lib/monad/sequence/member/static/mplus.ts
+++ src/lib/monad/sequence/member/static/mplus.ts
@@ -1,7 +1,5 @@
 import {Sequence} from '../../core';
 
 export default class <T, S> extends Sequence<T, S> {
-  public static mplus<T>(a: Sequence<T, any>, b: Sequence<T, any>): Sequence<T, [Sequence.Iterator<T>, Sequence.Iterator<T>]> {
-    return Sequence.mconcat([a, b]);
-  }
+  public static mplus = Sequence.mappend;
 }
