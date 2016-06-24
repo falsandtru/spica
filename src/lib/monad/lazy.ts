95003011b9f33bc5338ce8eeb87a8e7850fec910
falsandtru <falsandtru@users.noreply.github.com>
2016-06-25 01:47:16 +0900

    refactor(monad): change class member visibility

--- src/lib/monad/lazy.ts
+++ src/lib/monad/lazy.ts
@@ -1,7 +1,7 @@
 export abstract class Lazy<T> {
   constructor(protected thunk?: () => Lazy<T>) {
   }
-  protected memory_: this;
+  private memory_: this;
   protected evaluate(): this {
     return this.memory_ = this.memory_ || <this>this.thunk();
   }
