e87db21441ef392168fc99793daa601230a8faa7
falsandtru <falsandtru@users.noreply.github.com>
2016-06-28 17:29:45 +0900

    refactor(sequence): refactor Sequence.from static method

--- src/lib/monad/sequence/member/static/from.ts
+++ src/lib/monad/sequence/member/static/from.ts
@@ -2,6 +2,9 @@ import {Sequence} from '../../core';
 
 export default class <a, z> extends Sequence<a, z> {
   public static from<a>(as: a[]): Sequence<a, number> {
-    return new Sequence<a, number>((i = 0, cons) => i < as.length ? cons(as[i], ++i) : cons());
+    return new Sequence<a, number>((i = 0, cons) =>
+      i < as.length
+        ? cons(as[i], ++i)
+        : cons());
   }
 }
