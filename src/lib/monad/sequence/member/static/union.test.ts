43ad5b5bfef25947ce1a93ffeec583189caa5282
falsandtru <falsandtru@users.noreply.github.com>
2016-07-05 07:48:15 +0900

    feat(sequence): change Sequence.union static method signature

--- src/lib/monad/sequence/member/static/union.test.ts
+++ src/lib/monad/sequence/member/static/union.test.ts
@@ -8,52 +8,41 @@ describe('Unit: lib/monad/sequence/member/static/union', () => {
   describe('Sequence.union', () => {
     it('unlimited', () => {
       assert.deepStrictEqual(
-        Sequence.union([double, triple], (a, b) => a - b).take(7).read(),
+        Sequence.union(double, triple, (a, b) => a - b).take(7).read(),
         [0, 2, 3, 4, 6, 8, 9]);
       assert.deepStrictEqual(
-        Sequence.union([double.map(n => -n), triple.map(n => -n)], (a, b) => b - a).take(7).read(),
+        Sequence.union(double.map(n => -n), triple.map(n => -n), (a, b) => b - a).take(7).read(),
         [0, 2, 3, 4, 6, 8, 9].map(n => -n));
     });
 
     it('same', () => {
       assert.deepStrictEqual(
-        Sequence.union([double, triple], (a, b) => a - b).take(7).read(),
+        Sequence.union(double, triple, (a, b) => a - b).take(7).read(),
         [0, 2, 3, 4, 6, 8, 9]);
       assert.deepStrictEqual(
-        Sequence.union([double.map(n => -n), triple.map(n => -n)], (a, b) => b - a).take(7).read(),
+        Sequence.union(double.map(n => -n), triple.map(n => -n), (a, b) => b - a).take(7).read(),
         [0, 2, 3, 4, 6, 8, 9].map(n => -n));
     });
 
     it('mismatch', () => {
       assert.deepStrictEqual(
-        Sequence.union([double.dropWhile(n => n < 6).takeUntil(n => n === 12), triple], (a, b) => a - b).take(8).read(),
+        Sequence.union(double.dropWhile(n => n < 6).takeUntil(n => n === 12), triple, (a, b) => a - b).take(8).read(),
         [0, 3, 6, 8, 9, 10, 12, 15]);
       assert.deepStrictEqual(
-        Sequence.union([triple, double.dropWhile(n => n < 6).takeUntil(n => n === 12)], (a, b) => a - b).take(8).read(),
+        Sequence.union(triple, double.dropWhile(n => n < 6).takeUntil(n => n === 12), (a, b) => a - b).take(8).read(),
         [0, 3, 6, 8, 9, 10, 12, 15]);
     });
 
     it('empty', () => {
       assert.deepStrictEqual(
-        Sequence.union([nat, Sequence.from([])], (a, b) => a - b).take(3).read(),
+        Sequence.union(nat, Sequence.from([]), (a, b) => a - b).take(3).read(),
         [0, 1, 2]);
       assert.deepStrictEqual(
-        Sequence.union([Sequence.from([]), nat], (a, b) => a - b).take(3).read(),
+        Sequence.union(Sequence.from([]), nat, (a, b) => a - b).take(3).read(),
         [0, 1, 2]);
       assert.deepStrictEqual(
-        Sequence.union([Sequence.from([]), Sequence.from([])], (a, b) => a - b).take(3).read(),
+        Sequence.union(Sequence.from([]), Sequence.from([]), (a, b) => a - b).take(3).read(),
         []);
-    });
-
-    it('multiple', () => {
-      assert.deepStrictEqual(
-        Sequence.union([
-          new Sequence<number, number>((n = 0, cons) => cons(n, n + 2)),
-          new Sequence<number, number>((n = 0, cons) => cons(n, n + 3)),
-          new Sequence<number, number>((n = 0, cons) => cons(n, n + 5))
-        ], (a, b) => a - b)
-          .take(9).read(),
-        [0, 2, 3, 4, 5, 6, 8, 9, 10]);
     });
 
   });
--- src/lib/monad/sequence/member/static/union.ts
+++ src/lib/monad/sequence/member/static/union.ts
@@ -1,28 +1,24 @@
 import {Sequence} from '../../core';
 
 export default class <a, z> extends Sequence<a, z> {
-  public static union<a>(as: Sequence<a, any>[], cmp: (a: a, b: a) => number): Sequence<a, [Sequence.Iterator<a>, Sequence.Iterator<a>]> {
-    return as.reduce((a, b) => union(a, b, cmp));
+  public static union<a>(a: Sequence<a, any>, b: Sequence<a, any>, cmp: (a: a, b: a) => number): Sequence<a, [Sequence.Iterator<a>, Sequence.Iterator<a>]> {
+    return new Sequence<a, [Sequence.Iterator<a>, Sequence.Iterator<a>]>(([ai, bi] = [() => a.iterate(), () => b.iterate()], cons) =>
+      Sequence.Iterator.when(
+        ai(),
+        () =>
+          Sequence.Iterator.when(
+            bi(),
+            () => cons(),
+            bt => cons(Sequence.Thunk.value(bt), [Sequence.Iterator.done, Sequence.Thunk.iterator(bt)])),
+        at =>
+          Sequence.Iterator.when(
+            bi(),
+            () => cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), Sequence.Iterator.done]),
+            bt => {
+              const ord = cmp(Sequence.Thunk.value(at), Sequence.Thunk.value(bt));
+              if (ord < 0) return cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), bi]);
+              if (ord > 0) return cons(Sequence.Thunk.value(bt), [ai, Sequence.Thunk.iterator(bt)]);
+              return cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), Sequence.Thunk.iterator(bt)]);
+            })));
   }
-}
-
-function union<a>(a: Sequence<a, any>, b: Sequence<a, any>, cmp: (a: a, b: a) => number): Sequence<a, [Sequence.Iterator<a>, Sequence.Iterator<a>]> {
-  return new Sequence<a, [Sequence.Iterator<a>, Sequence.Iterator<a>]>(([ai, bi] = [() => a.iterate(), () => b.iterate()], cons) =>
-    Sequence.Iterator.when(
-      ai(),
-      () =>
-        Sequence.Iterator.when(
-          bi(),
-          () => cons(),
-          bt => cons(Sequence.Thunk.value(bt), [Sequence.Iterator.done, Sequence.Thunk.iterator(bt)])),
-      at =>
-        Sequence.Iterator.when(
-          bi(),
-          () => cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), Sequence.Iterator.done]),
-          bt => {
-            const result = cmp(Sequence.Thunk.value(at), Sequence.Thunk.value(bt));
-            if (result < 0) return cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), bi]);
-            if (result > 0) return cons(Sequence.Thunk.value(bt), [ai, Sequence.Thunk.iterator(bt)]);
-            return cons(Sequence.Thunk.value(at), [Sequence.Thunk.iterator(at), Sequence.Thunk.iterator(bt)]);
-          })));
 }
