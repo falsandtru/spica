6d71be9f372d5edcf73cd0ae41a5a4b89df87345
falsandtru <falsandtru@users.noreply.github.com>
2016-07-05 22:31:01 +0900

    refactor(sequence): refactor Sequence.scan method

--- src/lib/monad/sequence/member/instance/scan.ts
+++ src/lib/monad/sequence/member/instance/scan.ts
@@ -2,7 +2,7 @@ import {Sequence} from '../../core';
 
 export default class <a, z> extends Sequence<a, z> {
   public scan<b>(f: (b: b, a: a) => b, z: b): Sequence<b, [b, Sequence.Iterator<a>, number]> {
-    return new Sequence<b, [b, Sequence.Iterator<a>, number]>(([prev = z, iter = () => this.iterate(), i = 0] = [void 0, void 0, void 0]) =>
+    return new Sequence<b, [b, Sequence.Iterator<a>, number]>(([prev, iter, i] = [z, () => this.iterate(), 0]) =>
       Sequence.Iterator.when(
         iter(),
         () =>
