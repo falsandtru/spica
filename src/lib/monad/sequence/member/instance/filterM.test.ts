742962660bd0ec24a93ef9cdbd10828bfd5227aa
falsandtru <falsandtru@users.noreply.github.com>
2016-07-07 22:02:29 +0900

    test(sequence): improve Sequence.filterM method tests

--- src/lib/monad/sequence/member/instance/filterM.test.ts
+++ src/lib/monad/sequence/member/instance/filterM.test.ts
@@ -4,6 +4,15 @@ describe('Unit: lib/monad/sequence/member/filterM', () => {
   const nat = new Sequence<number, number>((n = 0, cons) => cons(n, n + 1));
 
   describe('filterM', () => {
+    it('0 []', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(0)
+          .filterM(() => Sequence.from([]))
+          .read(),
+        [[]]);
+    });
+
     it('0 [false]', () => {
       assert.deepStrictEqual(
         nat
@@ -29,6 +38,15 @@ describe('Unit: lib/monad/sequence/member/filterM', () => {
           .filterM(() => Sequence.from([true, false]))
           .read(),
         [[]]);
+    });
+
+    it('1 []', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(1)
+          .filterM(() => Sequence.from([]))
+          .read(),
+        []);
     });
 
     it('1 [false]', () => {
@@ -58,17 +76,44 @@ describe('Unit: lib/monad/sequence/member/filterM', () => {
         [[0], []]);
     });
 
-    it('2 [true] 1', () => {
+    it('1 [false, true]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(1)
+          .filterM(() => Sequence.from([false, true]))
+          .read(),
+        [[], [0]]);
+    });
+
+    it('2 []', () => {
       assert.deepStrictEqual(
         nat
           .take(2)
-          .filterM(() => Sequence.from([true, false]))
+          .filterM(() => Sequence.from([]))
           .take(1)
+          .read(),
+        []);
+    });
+
+    it('2 [true]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(2)
+          .filterM(() => Sequence.from([true]))
           .read(),
         [[0, 1]]);
     });
 
-    it('2 [true]', () => {
+    it('2 [false]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(2)
+          .filterM(() => Sequence.from([false]))
+          .read(),
+        [[]]);
+    });
+
+    it('2 [true, false]', () => {
       assert.deepStrictEqual(
         nat
           .take(2)
@@ -77,13 +122,49 @@ describe('Unit: lib/monad/sequence/member/filterM', () => {
         [[0, 1], [0], [1], []]);
     });
 
-    it('2 [true]', () => {
+    it('2 [false, true]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(2)
+          .filterM(() => Sequence.from([false, true]))
+          .read(),
+        [[], [1], [0], [0, 1]]);
+    });
+
+    it('3 [true]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(3)
+          .filterM(() => Sequence.from([true]))
+          .read(),
+        [[0, 1, 2]]);
+    });
+
+    it('3 [false]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(3)
+          .filterM(() => Sequence.from([false]))
+          .read(),
+        [[]]);
+    });
+
+    it('3 [true, false]', () => {
       assert.deepStrictEqual(
         nat
           .take(3)
           .filterM(() => Sequence.from([true, false]))
           .read(),
         [[0, 1, 2], [0, 1], [0, 2], [0], [1, 2], [1], [2], []]);
+    });
+
+    it('3 [false, true]', () => {
+      assert.deepStrictEqual(
+        nat
+          .take(3)
+          .filterM(() => Sequence.from([false, true]))
+          .read(),
+        [[], [2], [1], [1, 2], [0], [0, 2], [0, 1], [0, 1, 2]]);
     });
 
   });
