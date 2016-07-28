01a57140b2b5ae22b706890d39f7c60566b4b162
falsandtru <falsandtru@users.noreply.github.com>
2016-07-29 05:49:18 +0900

    chore(config): update the karma config

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function (config) {
       ]
     },
     autoWatch: true,
+    autoWatchBatchDelay: 500,
     browsers: ['Chrome'],
     browserNoActivityTimeout: 100 * 1e3
   });
