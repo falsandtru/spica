c47bbc2453b258601058466c69cc977056a564c1
falsandtru <falsandtru@users.noreply.github.com>
2016-11-22 00:24:37 +0900

    feat(package): add support for bundling using Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,6 @@ function compile(paths) {
     .on("error", err => done = console.log(err + ''))
     .pipe(source(`${pkg.name}.js`))
     .pipe(buffer())
-    .pipe($.derequire())
     .once("finish", () => done || process.exit(1));
 }
 
