df00b2bee6033df260505660147f005914895302
falsandtru <falsandtru@users.noreply.github.com>
2017-04-22 07:40:13 +0900

    chore(config): update dependencies

--- src/lib/curry.ts
+++ src/lib/curry.ts
@@ -1,9 +1,9 @@
 import { Curry } from '../../index.d';
 
-export const curry: Curry = <T>(f: () => T, ctx?: any) =>
+export const curry: Curry = <Curry>(<T>(f: () => T, ctx ?: any) =>
   f.length === 0
     ? () => f.call(ctx)
-    : curry_(f, [], ctx);
+    : curry_(f, [], ctx));
 
 function curry_(f: (...ys: any[]) => any, xs: any[], ctx: any) {
   return f.length <= xs.length
