ebe4fe64a668eb121ace139585b71f60330f293b
falsandtru <falsandtru@users.noreply.github.com>
2017-06-01 14:42:16 +0900

    fix(cancellation): fix state management

--- src/lib/cancellation.ts
+++ src/lib/cancellation.ts
@@ -1,47 +1,42 @@
-import { noop } from './noop';
 import { Maybe, Just, Nothing } from './monad/maybe';
 import { Either, Left, Right } from './monad/either';
 
 export class Cancellation<L = void> {
+  private done = false;
   private reason: L;
   private readonly listeners: Set<(reason: L) => void> = new Set();
-  public readonly register = (listener: (reason: L) => void): () => void =>
-    this.register_(listener);
-  private register_ = (listener: (reason: L) => void): () => void => {
+  public readonly register = (listener: (reason: L) => void) => {
+    if (this.canceled) return void listener(this.reason), () => void 0;
+    if (this.done) return () => void 0;
     void this.listeners.add(handler);
-    return () => void this.listeners.delete(handler);
+    return () =>
+      this.done
+        ? void 0
+        : void this.listeners.delete(handler);
 
     function handler(reason: L): void {
       void listener(reason);
     }
   };
-  public cancel: {
-    (reason: L): void;
-    (this: Cancellation<void>): void;
-  } = (reason?: L) => {
+  public readonly cancel = (reason?: L) => {
+    if (this.done) return;
+    this.done = true;
     this.canceled = true;
     this.reason = reason!;
-    this.register_ = cb => (
-      void cb(this.reason),
-      () => void 0);
-    this.cancel = noop;
-    this.close = noop;
+    const listeners = Array.from(this.listeners);
     void Object.freeze(this);
-    while (this.listeners.size > 0) {
-      void this.listeners
-        .forEach(cb => (
-          void this.listeners.delete(cb),
-          void cb(reason!)));
-    }
+    void Object.freeze(this.listeners);
+    void listeners
+      .forEach(cb =>
+        void cb(reason!));
+  };
+  public readonly close = () => {
+    if (this.done) return;
+    this.done = true;
+    void this.listeners.clear();
+    void Object.freeze(this);
     void Object.freeze(this.listeners);
   };
-  public close = (): void => (
-    this.register_ = () => () => void 0,
-    this.cancel = noop,
-    this.close = noop,
-    void this.listeners.clear(),
-    void Object.freeze(this),
-    void Object.freeze(this.listeners));
   public canceled = false;
   public readonly promise = <T>(val: T): Promise<T> =>
     this.canceled
