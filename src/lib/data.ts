fac2dcfa492bf548945e8ab3a8a36c2294a4c3a7
falsandtru <falsandtru@users.noreply.github.com>
2017-11-19 00:36:24 +0900

    fix(data): don't refer package definitions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,1 +1,8 @@
-export { Diff, Overwrite } from '../../index.d';
+type Compose<T, U> = Pick<T & U, keyof T | keyof U>;
+export type Diff<T, U> = Pick<T, DiffKey<keyof T, keyof U>>;
+export type Overwrite<T, U> = Compose<{ [P in DiffKey<keyof T, keyof U>]: T[P]; }, U>;
+type DiffKey<T extends string, U extends string> = (
+  & { [P in T]: P; }
+  & { [P in U]: never; }
+  & { [x: string]: never; }
+)[T];
