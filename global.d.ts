ce4d8b3d8e74813ed2c00afb4b4e23d96052cc80
falsandtru <falsandtru@users.noreply.github.com>
2018-03-29 18:44:01 +0900

    refactor: split global declaration file

--- global.d.ts
+++ global.d.ts
@@ -1,27 +1,14 @@
-declare global {
-  interface NumberConstructor {
-    isNaN(target: any): boolean;
-  }
-  function setTimeout(handler: (...args: any[]) => void, timeout?: number, ...args: any[]): number;
-  var console: Console;
-  interface Console {
-    assert(value: any, message?: string, ...optionalParams: any[]): void;
-    dir(obj: any, options?: { showHidden?: boolean, depth?: number, colors?: boolean }): void;
-    error(message?: any, ...optionalParams: any[]): void;
-    info(message?: any, ...optionalParams: any[]): void;
-    log(message?: any, ...optionalParams: any[]): void;
-    debug(message?: any, ...optionalParams: any[]): void;
-    time(label: string): void;
-    timeEnd(label: string): void;
-    trace(message?: any, ...optionalParams: any[]): void;
-    warn(message?: any, ...optionalParams: any[]): void;
-  }
-}
-
-import assert from 'power-assert';
-
-type Assert = typeof assert;
-
-declare global {
-  const assert: Assert;
+declare function setTimeout(handler: (...args: any[]) => void, timeout?: number, ...args: any[]): number;
+declare var console: Console;
+interface Console {
+  assert(value: any, message?: string, ...optionalParams: any[]): void;
+  dir(obj: any, options?: { showHidden?: boolean, depth?: number, colors?: boolean }): void;
+  error(message?: any, ...optionalParams: any[]): void;
+  info(message?: any, ...optionalParams: any[]): void;
+  log(message?: any, ...optionalParams: any[]): void;
+  debug(message?: any, ...optionalParams: any[]): void;
+  time(label: string): void;
+  timeEnd(label: string): void;
+  trace(message?: any, ...optionalParams: any[]): void;
+  warn(message?: any, ...optionalParams: any[]): void;
 }
