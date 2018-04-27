421c41df6e2e7da75e340c24e84695cd9451327a
falsandtru <falsandtru@users.noreply.github.com>
2018-04-27 14:46:57 +0900

    refactor: refactoring

--- src/coroutine.ts
+++ src/coroutine.ts
@@ -1,6 +1,5 @@
 import { Future } from './future'; 
 import { If, TEq } from './type';
-import { tick } from './tick'; 
 
 type Result<T, S> = If<TEq<Exclude<T, S>, never>, T, Exclude<T, S>>;
 
@@ -12,55 +11,56 @@ export class Coroutine<T, S = void> extends Promise<Result<T, S>> implements Asy
   }
   constructor(
     gen: (this: Coroutine<T, S>) => Iterator<T | S> | AsyncIterator<T | S>,
+    resume?: () => Promise<void>);
+  constructor(
+    gen: (this: Coroutine<T, S>) => Iterator<T | S> | AsyncIterator<T | S>,
     resume: () => Promise<void> = () => clock,
+    private readonly result = new Future<Result<T, S>>()
   ) {
     super((resolve, reject) =>
-      void tick(() => void this.result.then(resolve, reject)));
-    const iter = gen.call(this) as ReturnType<typeof gen>;
+      void result.then(resolve, reject));
     void (async () => {
       try {
+        const iter = gen.call(this) as ReturnType<typeof gen>;
         while (this.alive) {
           const { value, done } = await iter.next();
-          if (!this.alive) break;
+          if (!this.alive) return;
           if (!done) {
             const state = this.state.bind({ value: await value as S, done });
             assert(state === this.state);
-            if (!this.alive) break;
+            if (!this.alive) return;
             this.state = new Future();
             await state;
-            if (!this.alive) break;
+            if (!this.alive) return;
             await (this.clock = resume());
             continue;
           }
           else {
             this.alive = false;
             await this.state.bind({ value: undefined as any as S, done });
-            void this.result.bind(value as Result<T, S>);
+            void result.bind(value as Result<T, S>);
           }
         }
       }
-      catch (e) {
-        void this.terminate(e);
+      catch (reason) {
+        void this.terminate(reason);
       }
     })();
   }
   private alive = true;
-  private clock!: Promise<void>;
+  private clock = clock;
   private state = new Future<IteratorResult<S>>();
-  private readonly result = new Future<Result<T, S>>();
   public terminate(reason?: any): void {
     if (!this.alive) return;
     this.alive = false;
-    void (async () => {
-      await this.state.bind({ value: undefined as any as S, done: true });
-      void this.result.bind(Promise.reject(reason));
-    })();
+    void this.state.bind({ value: undefined as any as S, done: true })
+      .then(() => void this.result.bind(Promise.reject(reason)));
   }
   public [Symbol.asyncIterator] = async function* (this: Coroutine<T, S>): AsyncIterableIterator<S> {
     await this.clock; // Skip the current state if it's already resolved.
     while (this.alive) {
       const { value, done } = await this.state;
-      if (done) break;
+      if (done) return;
       yield value;
     }
   }
