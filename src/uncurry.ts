3f3ec68290f6863e11311cbc82f4e9f99334b227
falsandtru <falsandtru@users.noreply.github.com>
2019-01-30 01:35:15 +0900

    refactor: refactoring

--- src/uncurry.ts
+++ src/uncurry.ts
@@ -1,11 +1,11 @@
 interface Uncurry {
-  <z>(f: () => z): (a: []) => z;
-  <a, z>(f: (a: a) => z): (a: [a]) => z;
-  <a, b, z>(f: (a: a, b: b) => z): (a: [a, b]) => z;
-  <a, b, c, z>(f: (a: a, b: b, c: c) => z): (a: [a, b, c]) => z;
-  <a, b, c, d, z>(f: (a: a, b: b, c: c, d: d) => z): (a: [a, b, c, d]) => z;
-  <a, b, c, d, e, z>(f: (a: a, b: b, c: c, d: d, e: e) => z): (a: [a, b, c, d, e]) => z;
+  <z>(f: () => z): (xs: []) => z;
+  <a, z>(f: (a: a) => z): (xs: [a]) => z;
+  <a, b, z>(f: (a: a, b: b) => z): (xs: [a, b]) => z;
+  <a, b, c, z>(f: (a: a, b: b, c: c) => z): (xs: [a, b, c]) => z;
+  <a, b, c, d, z>(f: (a: a, b: b, c: c, d: d) => z): (xs: [a, b, c, d]) => z;
+  <a, b, c, d, e, z>(f: (a: a, b: b, c: c, d: d, e: e) => z): (xs: [a, b, c, d, e]) => z;
 }
 
-export const uncurry: Uncurry = <Uncurry>(<a, z>(f: (...as: a[]) => z) =>
-  (as: a[]) => f(...as));
+export const uncurry: Uncurry = <Uncurry>(<x, z>(f: (...xs: x[]) => z) =>
+  (xs: x[]) => f(...xs));
