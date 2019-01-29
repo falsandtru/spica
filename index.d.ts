75bd595f26502f351ef1280c51512555b40f5056
falsandtru <falsandtru@users.noreply.github.com>
2019-01-29 23:39:27 +0900

    refactor: refactoring

--- index.d.ts
+++ index.d.ts
@@ -482,12 +482,12 @@ interface Curried5<a, b, c, d, e, z> {
   (a: a): Curried4<b, c, d, e, z>;
 }
 interface Curry {
-  <z>(f: () => z, ctx?: any): Curried0<z>;
-  <a, z>(f: (a: a) => z, ctx?: any): Curried1<a, z>;
-  <a, b, z>(f: (a: a, b: b) => z, ctx?: any): Curried2<a, b, z>;
-  <a, b, c, z>(f: (a: a, b: b, c: c) => z, ctx?: any): Curried3<a, b, c, z>;
-  <a, b, c, d, z>(f: (a: a, b: b, c: c, d: d) => z, ctx?: any): Curried4<a, b, c, d, z>;
-  <a, b, c, d, e, z>(f: (a: a, b: b, c: c, d: d, e: e) => z, ctx?: any): Curried5<a, b, c, d, e, z>;
+  <z>(f: () => z): Curried0<z>;
+  <a, z>(f: (a: a) => z): Curried1<a, z>;
+  <a, b, z>(f: (a: a, b: b) => z): Curried2<a, b, z>;
+  <a, b, c, z>(f: (a: a, b: b, c: c) => z): Curried3<a, b, c, z>;
+  <a, b, c, d, z>(f: (a: a, b: b, c: c, d: d) => z): Curried4<a, b, c, d, z>;
+  <a, b, c, d, e, z>(f: (a: a, b: b, c: c, d: d, e: e) => z): Curried5<a, b, c, d, e, z>;
 }
 export const curry: Curry;
 
