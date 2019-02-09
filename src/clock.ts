a5d39d00c9c7959faf4a15b844cb8cd68a062947
falsandtru <falsandtru@users.noreply.github.com>
2019-02-09 13:25:43 +0900

    refactor: refactoring

--- src/clock.ts
+++ src/clock.ts
@@ -6,6 +6,6 @@ export const clock: Promise<void> = Promise.resolve();
 export function wait(ms: number): AtomicPromise<void> {
   assert(ms >= 0);
   return ms === 0
-    ? AtomicPromise.resolve()
+    ? AtomicPromise.resolve(clock)
     : new AtomicPromise(resolve => void setTimeout(resolve, ms));
 }
