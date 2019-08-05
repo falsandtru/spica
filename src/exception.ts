baf9c95401f4fb0a751264ed8bff1aac611102d8
falsandtru <falsandtru@users.noreply.github.com>
2019-08-06 01:53:00 +0900

    refactor: refactoring

--- src/exception.ts
+++ src/exception.ts
@@ -1,18 +1,5 @@
 export function causeAsyncException(reason: unknown): void {
-  assert(!void console.debug(stringify(reason)));
+  assert(!void console.error(reason));
   void new Promise((_, reject) =>
     void reject(reason));
-}
-
-function stringify(target: any): string {
-  try {
-    return target instanceof Error && typeof target.stack === 'string'
-      ? target.stack
-      : target !== undefined && target !== null && typeof target.toString === 'function'
-        ? target + ''
-        : Object.prototype.toString.call(target);
-  }
-  catch (reason) {
-    return stringify(reason);
-  }
 }
