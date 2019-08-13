970d86a31b9287a26cb46fe18264b73217335419
falsandtru <falsandtru@users.noreply.github.com>
2019-08-13 21:51:50 +0900

    refactor: refactoring

--- src/exception.ts
+++ src/exception.ts
@@ -1,5 +1,4 @@
 export function causeAsyncException(reason: unknown): void {
   assert(!void console.error(reason));
-  void new Promise((_, reject) =>
-    void reject(reason));
+  void Promise.reject(reason);
 }
