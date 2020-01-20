b1a66c0262ffe27ae948c8e1cdeae0a3430e8273
falsandtru <falsandtru@users.noreply.github.com>
2020-01-21 02:06:07 +0900

    refactor: refactoring

--- benchmark/promise.ts
+++ benchmark/promise.ts
@@ -2,7 +2,7 @@ import { benchmark } from './benchmark';
 import { AtomicPromise } from '../';
 
 describe('Benchmark:', function () {
-  this.timeout(10 * 1e3);
+  this.timeout(20 * 1e3);
 
   describe('AtomicPromise', function () {
     it('new', function (done) {
