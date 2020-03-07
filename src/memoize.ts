bdd4e577804f54e6ab1fe804079b088f5e51957b
falsandtru <falsandtru@users.noreply.github.com>
2020-03-07 22:00:23 +0900

    Refactoring

--- src/memoize.ts
+++ src/memoize.ts
@@ -2,10 +2,12 @@ import { undefined } from './global';
 import { Collection } from './collection';
 
 export function memoize<a, z>(f: (a: a) => z, memory: Collection<a, z> = new Map()): typeof f {
+  let nullable = false;
   return a => {
     let z = memory.get(a);
-    if (z !== undefined || memory.has(a)) return z!;
+    if (z !== undefined || nullable && memory.has(a)) return z!;
     z = f(a);
+    nullable = nullable || z === undefined;
     memory.set(a, z);
     return z;
   };
