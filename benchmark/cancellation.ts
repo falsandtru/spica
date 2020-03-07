fd013188ffc384f2c198eaf0a678a14338dc10b5
falsandtru <falsandtru@users.noreply.github.com>
2020-03-07 20:00:44 +0900

    Refactoring

--- benchmark/cancellation.ts
+++ benchmark/cancellation.ts
@@ -9,9 +9,58 @@ describe('Benchmark:', function () {
       benchmark('Cancellation new', () => new Cancellation(), done);
     });
 
-    it('cancel', function (done) {
-      benchmark('Cancellation cancel', () => {
+    it('register 1', function (done) {
+      benchmark('Cancellation register 1', () => {
         const c = new Cancellation();
+        c.register(() => 0);
+      }, done);
+    });
+
+    it('register 3', function (done) {
+      benchmark('Cancellation register 3', () => {
+        const c = new Cancellation();
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+      }, done);
+    });
+
+    it('register 5', function (done) {
+      benchmark('Cancellation register 5', () => {
+        const c = new Cancellation();
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+      }, done);
+    });
+
+    it('cancel 1', function (done) {
+      benchmark('Cancellation cancel 1', () => {
+        const c = new Cancellation();
+        c.register(() => 0);
+        c.cancel();
+      }, done);
+    });
+
+    it('cancel 3', function (done) {
+      benchmark('Cancellation cancel 3', () => {
+        const c = new Cancellation();
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+        c.cancel();
+      }, done);
+    });
+
+    it('cancel 5', function (done) {
+      benchmark('Cancellation cancel 5', () => {
+        const c = new Cancellation();
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
+        c.register(() => 0);
         c.register(() => 0);
         c.cancel();
       }, done);
