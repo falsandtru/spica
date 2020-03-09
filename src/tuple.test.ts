b94357dad205c0cd530cc44a70e99ff8f31b56a4
falsandtru <falsandtru@users.noreply.github.com>
2020-03-09 11:02:29 +0900

    Refactoring

--- src/tuple.test.ts
+++ src/tuple.test.ts
@@ -3,8 +3,8 @@ import { tuple } from './tuple';
 describe('Unit: lib/tuple', () => {
   describe('tuple', () => {
     it('', () => {
-      assert((): [number] => tuple([0]));
-      assert((): [number, number] => tuple([0, 0]));
+      assert((): [number] => tuple(0));
+      assert((): [number, number] => tuple(0, 0));
     });
 
   });
