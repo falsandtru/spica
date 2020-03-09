4b62fc05121591cc97a2d1a27ffec4792bd3b15b
falsandtru <falsandtru@users.noreply.github.com>
2020-03-10 04:18:20 +0900

    Refactoring

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,10 +1,15 @@
-import { undefined } from './global';
+import { undefined, Map } from './global';
 import { Collection } from './collection';
 
-export function memoize<f extends (...as: [unknown, ...unknown[]]) => unknown, b = Parameters<f>[0]>(f: f, memory?: Collection<b, ReturnType<f>>, identify?: (...as: Parameters<f>) => b): f;
-export function memoize<a, z, b = a>(f: (a: a) => z, memory?: Collection<b, z>, identify?: (a: a) => b): typeof f;
-export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, memory?: Collection<b, z>, identify?: (...as: as) => b): typeof f;
-export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, memory: Collection<b, z> = new Map(), identify: (...as: as) => b = (...as) => as[0] as b): typeof f {
+export function memoize<f extends (...as: [unknown, ...unknown[]]) => unknown, b = Parameters<f>[0]>(f: f, memory?: Collection<b, ReturnType<f>>): f;
+export function memoize<f extends (...as: [unknown, ...unknown[]]) => unknown, b = Parameters<f>[0]>(f: f, identify?: (...as: Parameters<f>) => b, memory?: Collection<b, ReturnType<f>>): f;
+export function memoize<a, z, b = a>(f: (a: a) => z, memory?: Collection<b, z>): typeof f;
+export function memoize<a, z, b = a>(f: (a: a) => z, identify?: (a: a) => b, memory?: Collection<b, z>): typeof f;
+export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, memory?: Collection<b, z>): typeof f;
+export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, identify?: (...as: as) => b, memory?: Collection<b, z>): typeof f;
+export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, identify: Collection<b, z> | ((...as: as) => b) = (...as) => as[0] as b, memory?: Collection<b, z>): typeof f {
+  if (typeof identify === 'object') return memoize(f, undefined, identify);
+  if (memory === undefined) return memoize(f, undefined, new Map());
   let nullish = false;
   return (...as) => {
     const b = identify(...as);
