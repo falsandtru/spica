adac66462bd14b81e20532f6d57e932854f05d77
falsandtru <falsandtru@users.noreply.github.com>
2020-03-10 03:55:32 +0900

    Refactoring

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,8 +1,8 @@
 import { undefined } from './global';
 import { Collection } from './collection';
 
-export function memoize<a, z, b = a>(f: (a: a) => z, memory?: Collection<b, z>, identify?: (a: a) => b): typeof f;
 export function memoize<f extends (...as: [unknown, ...unknown[]]) => unknown, b = Parameters<f>[0]>(f: f, memory?: Collection<b, ReturnType<f>>, identify?: (...as: Parameters<f>) => b): f;
+export function memoize<a, z, b = a>(f: (a: a) => z, memory?: Collection<b, z>, identify?: (a: a) => b): typeof f;
 export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, memory?: Collection<b, z>, identify?: (...as: as) => b): typeof f;
 export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, memory: Collection<b, z> = new Map(), identify: (...as: as) => b = (...as) => as[0] as b): typeof f {
   let nullish = false;
