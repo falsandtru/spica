37a502aeae2fe4f838e7f81309a8af96f8a60933
falsandtru <falsandtru@users.noreply.github.com>
2020-03-10 04:45:53 +0900

    Refactoring

--- src/memoize.ts
+++ src/memoize.ts
@@ -9,7 +9,7 @@ export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (..
 export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, identify?: (...as: as) => b, memory?: Collection<b, z>): typeof f;
 export function memoize<as extends [unknown, ...unknown[]], z, b = as[0]>(f: (...as: as) => z, identify: Collection<b, z> | ((...as: as) => b) = (...as) => as[0] as b, memory?: Collection<b, z>): typeof f {
   if (typeof identify === 'object') return memoize(f, undefined, identify);
-  if (memory === undefined) return memoize(f, undefined, new Map());
+  if (memory === undefined) return memoize(f, identify, new Map());
   let nullish = false;
   return (...as) => {
     const b = identify(...as);
