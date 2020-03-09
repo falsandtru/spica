d0c3035d2be9f1d79e07a34aca21a8dc40822cad
falsandtru <falsandtru@users.noreply.github.com>
2020-03-09 15:23:05 +0900

    Refactoring

--- src/sqid.ts
+++ src/sqid.ts
@@ -6,14 +6,14 @@ let cnt = 0;
 export function sqid(): string
 export function sqid(id: number): string
 export function sqid(id?: number): string {
-  if (arguments.length > 0) {
+  if (arguments.length === 0) {
+    if (cnt === Number.MAX_SAFE_INTEGER) throw new TypeError(`Spica: sqid: sqid reached max safe integer.`);
+    return sqid(++cnt)
+  }
+  else {
     if (typeof id !== 'number') throw new TypeError(`Spica: sqid: A parameter value must be a number: ${id}`);
     if (id >= 0 === false) throw new TypeError(`Spica: sqid: A parameter value must be a positive number: ${id}`);
     if (id % 1 !== 0) throw new TypeError(`Spica: sqid: A parameter value must be an integer: ${id}`);
     return (zeros + id).slice(-16);
-  }
-  else {
-    if (cnt === Number.MAX_SAFE_INTEGER) throw new TypeError(`Spica: sqid: sqid reached max safe integer.`);
-    return sqid(++cnt)
   }
 }
