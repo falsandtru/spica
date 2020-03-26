8f69e09a98ff501a6cc4cc0b0e1b70807144e95e
falsandtru <falsandtru@users.noreply.github.com>
2020-03-26 18:42:18 +0900

    Refactoring

--- src/copropagator.ts
+++ src/copropagator.ts
@@ -1,14 +1,14 @@
+import { Error } from './global';
 import { Coroutine } from './coroutine';
 import { AtomicPromise } from './promise';
 import { never } from './clock';
-import { Error } from './global';
 
-export class Copropagator<T, R = T, S = unknown> extends Coroutine<T, R, S> {
+export class Copropagator<T = unknown, R = T, S = unknown> extends Coroutine<T, R, S> {
   constructor(
     coroutines: Iterable<Coroutine<T, R, S>>,
     reducer: (results: T[]) => T = results => results[0],
   ) {
-    super(async function* (): AsyncGenerator<R, T, S> {
+    super(async function* () {
       void this.then(
         result => {
           for (const co of coroutines) {
