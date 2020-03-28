e51e8fa1657ef40c7142ac9a55dac4da914e9ec4
falsandtru <falsandtru@users.noreply.github.com>
2020-03-29 07:04:12 +0900

    Refactoring

--- src/channel.ts
+++ src/channel.ts
@@ -10,30 +10,30 @@ type Channel =
   | (() => AsyncIterable<unknown, unknown, undefined>);
 type ChannelResult<T extends Record<string, Channel>> =
   { [P in keyof T]: readonly [P, ChannelIteratorResult<T[P]>]; }[keyof T];
-type ChannelIteratorResult<G extends Channel> =
-  G extends () => AsyncIterable<infer T, infer U> ? IteratorResult<T, U> :
-  G extends AsyncIterable<infer T, infer U> ? IteratorResult<T, U> :
+type ChannelIteratorResult<C extends Channel> =
+  C extends () => AsyncIterable<infer T, infer U> ? IteratorResult<T, U> :
+  C extends AsyncIterable<infer T, infer U> ? IteratorResult<T, U> :
   never;
 
 export async function* select<T extends Record<string, Channel>>(
   channels: T,
 ): AsyncGenerator<ChannelResult<T>, undefined, undefined> {
-  const jobs = new Set(ObjectEntries(channels)
+  const reqs = new Set(ObjectEntries(channels)
     .map(([name, chan]) => (
       chan = typeof chan === 'function' ? chan() : chan,
       take(name, chan[Symbol.asyncIterator]()))));
-  while (jobs.size > 0) {
-    const [name, chan, job, result] = await Promise.race(jobs);
-    assert(jobs.has(job));
-    void jobs.delete(job);
-    !result.done && void jobs.add(take(name, chan));
+  while (reqs.size > 0) {
+    const [name, chan, req, result] = await Promise.race(reqs);
+    assert(reqs.has(req));
+    void reqs.delete(req);
+    !result.done && void reqs.add(take(name, chan));
     yield [name, result as ChannelIteratorResult<T[keyof T]>];
   }
   return;
 }
 
-type Job = Promise<readonly [string, AsyncIterator<unknown, unknown, undefined>, Job, IteratorResult<unknown, unknown>]>;
-function take(name: string, chan: AsyncIterator<unknown, unknown, undefined>): Job {
-  const job: Job = chan.next().then(result => [name, chan, job, result]);
-  return job;
+type Request = Promise<readonly [string, AsyncIterator<unknown, unknown, undefined>, Request, IteratorResult<unknown, unknown>]>;
+function take(name: string, chan: AsyncIterator<unknown, unknown, undefined>): Request {
+  const req: Request = chan.next().then(result => [name, chan, req, result]);
+  return req;
 }
