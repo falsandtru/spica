1a27b1a59b3baeef7ec18519bb54ed101c67221d
falsandtru <falsandtru@users.noreply.github.com>
2020-10-12 02:29:20 +0900

    Fix breaks on dependents

--- src/global.ts
+++ src/global.ts
@@ -3,6 +3,7 @@ const global: typeof globalThis & { undefined: undefined } = void 0
   || typeof globalThis !== 'undefined' && globalThis
   || typeof self !== 'undefined' && self
   || Function('return this')();
-global.global = global;
+// `global.global = global` breaks the build on the dependent projects.
+eval('global.global = global');
 // Don't provide types.
 export = global;
