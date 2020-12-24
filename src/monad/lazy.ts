b0b34a65ce4b2f6665412457a0c3ebabc0ce7630
falsandtru <falsandtru@users.noreply.github.com>
2020-12-24 17:54:46 +0900

    Refactoring

--- src/monad/lazy.ts
+++ src/monad/lazy.ts
@@ -1,9 +1,13 @@
+import { undefined } from '../global';
+
 export abstract class Lazy<a> {
   constructor(protected readonly thunk: () => Lazy<a>) {
   }
-  private memory_?: this;
+  private memory_?: this = undefined;
   protected evaluate(): this {
-    return this.memory_ = this.memory_ || this.thunk!() as this;
+    return this.memory_
+      ? this.memory_
+      : this.memory_ = this.thunk() as this;
   }
-  public abstract extract(): any;
+  public abstract extract(): unknown;
 }
