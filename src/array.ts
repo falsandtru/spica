50430a1311dddb6441ef12c4315622654c70c8c4
falsandtru <falsandtru@users.noreply.github.com>
2021-01-03 00:11:17 +0900

    Refactoring

--- src/array.ts
+++ src/array.ts
@@ -17,8 +17,8 @@ export function shift<a>(as: a[], count?: number): [a | undefined | a[], a[]] {
     ? [as.shift(), as]
     : [splice(as, 0, count), as];
 }
-export function unshift<a>(as: Iterable<a>, bs: a[]): a[] {
-  if (isArray(as)) {
+export function unshift<a>(as: Iterable<a> | ArrayLike<a>, bs: a[]): a[] {
+  if ('length' in as || isArray(as)) {
     for (let i = as.length - 1; i >= 0; --i) {
       bs.unshift(as[i]);
     }
@@ -38,14 +38,16 @@ export function pop<a>(as: a[], count?: number): [a[], a | undefined | a[]] {
     ? [as, as.pop()]
     : [as, splice(as, as.length - count, count)];
 }
-export function push<a>(as: a[], bs: Iterable<a>): a[] {
-  if (isArray(bs)) {
+export function push<a>(as: a[], bs: Iterable<a> | ArrayLike<a>): a[] {
+  if ('length' in bs || isArray(bs)) {
     for (let i = 0; i < bs.length; ++i) {
       as.push(bs[i]);
     }
   }
   else {
-    as.push(...bs);
+    for (const b of bs) {
+      as.push(b);
+    }
   }
   return as;
 }
