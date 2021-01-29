b6a2638e0d5bd8db58a6307c184214ebc4152161
falsandtru <falsandtru@users.noreply.github.com>
2021-01-29 12:10:56 +0900

    Refactoring

--- benchmark/cache.ts
+++ benchmark/cache.ts
@@ -9,73 +9,115 @@ describe('Benchmark:', function () {
       benchmark('Cache new', () => new Cache(1000), done);
     });
 
-    it('put 10', function (done) {
-      const capacity = 10;
-      const cache = new Cache<number, number>(capacity);
-      let i = 0;
-      benchmark('Cache put 10', () => cache.put(++i % (capacity * 3), i), done);
-    });
-
-    it('put 100', function (done) {
-      const capacity = 100;
-      const cache = new Cache<number, number>(capacity);
-      let i = 0;
-      benchmark('Cache put 100', () => cache.put(++i % (capacity * 3), i), done);
-    });
-
-    it('put 1000', function (done) {
-      const capacity = 1000;
-      const cache = new Cache<number, number>(capacity);
-      let i = 0;
-      benchmark('Cache put 1000', () => cache.put(++i % (capacity * 3), i), done);
-    });
-
-    it('get 10', function (done) {
+    it('put 10 100%', function (done) {
       const capacity = 10;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache get 10', () => cache.get(++i % (capacity * 3)), done);
+      benchmark('Cache put 10 100%', () => cache.put(++i % capacity, 0), done);
     });
 
-    it('get 100', function (done) {
+    it('put 10 0%', function (done) {
+      const capacity = 10;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache put 10 0%', () => cache.put(-1, 0), done);
+    });
+
+    it('put 100 100%', function (done) {
       const capacity = 100;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache get 100', () => cache.get(++i % (capacity * 3)), done);
+      benchmark('Cache put 100 100%', () => cache.put(++i % capacity, 0), done);
     });
 
-    it('get 1000', function (done) {
+    it('put 100 0%', function (done) {
+      const capacity = 100;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache put 100 0%', () => cache.put(-1, 0), done);
+    });
+
+    it('put 1000 100%', function (done) {
       const capacity = 1000;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache get 1000', () => cache.get(++i % (capacity * 3)), done);
+      benchmark('Cache put 1000 100%', () => cache.put(++i % capacity, 0), done);
     });
 
-    it('has 10', function (done) {
+    it('put 1000 0%', function (done) {
+      const capacity = 1000;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache put 1000 0%', () => cache.put(-1, 0), done);
+    });
+
+    it('get 10 100%', function (done) {
       const capacity = 10;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache has 10', () => cache.has(++i % (capacity * 3)), done);
+      benchmark('Cache get 10 100%', () => cache.get(++i % capacity), done);
     });
 
-    it('has 100', function (done) {
+    it('get 10 0%', function (done) {
+      const capacity = 10;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache get 10 0%', () => cache.get(-1), done);
+    });
+
+    it('get 100 100%', function (done) {
       const capacity = 100;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache has 100', () => cache.has(++i % (capacity * 3)), done);
+      benchmark('Cache get 100 100%', () => cache.get(++i % capacity), done);
     });
 
-    it('has 1000', function (done) {
+    it('get 100 0%', function (done) {
+      const capacity = 100;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache get 100 0%', () => cache.get(-1), done);
+    });
+
+    it('get 1000 100%', function (done) {
       const capacity = 1000;
       const cache = new Cache<number, number>(capacity);
       for (let i = 0; i < capacity; ++i) cache.put(i, i);
       let i = 0;
-      benchmark('Cache has 1000', () => cache.has(++i % (capacity * 3)), done);
+      benchmark('Cache get 1000 100%', () => cache.get(++i % capacity), done);
+    });
+
+    it('get 1000 0%', function (done) {
+      const capacity = 1000;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache get 1000 0%', () => cache.get(-1), done);
+    });
+
+    it('has 10 0%', function (done) {
+      const capacity = 10;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache has 10 0%', () => cache.has(-1), done);
+    });
+
+    it('has 100 0%', function (done) {
+      const capacity = 100;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache has 100 0%', () => cache.has(-1), done);
+    });
+
+    it('has 1000 0%', function (done) {
+      const capacity = 1000;
+      const cache = new Cache<number, number>(capacity);
+      for (let i = 0; i < capacity; ++i) cache.put(i, i);
+      benchmark('Cache has 1000 0%', () => cache.has(-1), done);
     });
 
   });
