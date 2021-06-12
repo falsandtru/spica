ab93699de8a1f14b8cb19ba826c5e274e8cf773a
falsandtru <falsandtru@users.noreply.github.com>
2021-06-12 18:31:03 +0900

    Refactoring

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,10 +1,11 @@
 import { setTimeout } from './global';
+import { causeAsyncException } from './exception';
 
 export function throttle<T, C = unknown>(interval: number, callback: (this: C, last: T, buffer: T[]) => void, capacity: number = 1): (this: C, arg: T) => void {
   // Bug: Karma and TypeScript
   let timer: ReturnType<typeof setTimeout> | 0 = 0;
   let buffer: T[] = [];
-  return function (this: unknown, data: T) {
+  return function self(this: unknown, data: T) {
     if (capacity === 1) {
       buffer = [data];
     }
@@ -13,14 +14,20 @@ export function throttle<T, C = unknown>(interval: number, callback: (this: C, l
       buffer.unshift(data);
     }
     if (timer !== 0) return;
-    timer = setTimeout(() => {
+    timer = setTimeout(async () => {
       assert(timer !== 0);
       assert(buffer.length > 0);
-      timer = 0;
       const buf = buffer;
       buffer = [];
       assert(buf.length > 0);
-      void callback.call(this, buf[0], buf);
+      try {
+        await callback.call(this, buf[0], buf);
+      }
+      catch (reason) {
+        causeAsyncException(reason);
+      }
+      timer = 0;
+      buffer.length > 0 && self.call(this, buffer.shift()!);
     }, interval);
   };
 }
@@ -29,7 +36,8 @@ export function debounce<T, C = unknown>(delay: number, callback: (this: C, last
   // Bug: Karma and TypeScript
   let timer: ReturnType<typeof setTimeout> | 0 = 0;
   let buffer: T[] = [];
-  return function (this: unknown, data: T) {
+  let callable = true;
+  return function self(this: unknown, data: T) {
     if (capacity === 1) {
       buffer = [data];
     }
@@ -42,14 +50,23 @@ export function debounce<T, C = unknown>(delay: number, callback: (this: C, last
       assert(timer !== 0);
       assert(buffer.length > 0);
       timer = 0;
-      void setTimeout(() => {
+      void setTimeout(async () => {
         if (timer !== 0) return;
+        if (!callable) return;
         assert(buffer.length > 0);
         const buf = buffer;
         buffer = [];
         assert(buf.length > 0);
-        void callback.call(this, buf[0], buf);
-      }, buffer.length > 1 ? delay : 0);
+        callable = false;
+        try {
+          await callback.call(this, buf[0], buf);
+        }
+        catch (reason) {
+          causeAsyncException(reason);
+        }
+        callable = true;
+        timer === 0 && buffer.length > 0 && self.call(this, buffer.shift()!);
+      }, delay);
     }, delay);
   };
 }
