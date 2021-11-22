5d9140c65287bfe0eb04a758feb73dbf28ecec82
falsandtru <falsandtru@users.noreply.github.com>
2021-11-23 06:40:30 +0900

    Refactoring

--- src/arrow.ts
+++ src/arrow.ts
@@ -2,13 +2,17 @@ type Functions2Parameters<FS extends readonly ((..._: unknown[]) => unknown)[]> 
 type Functions2Returns<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? ReturnType<FS[P]> : never; };
 type Functions2Context<FS extends readonly ((..._: unknown[]) => unknown)[]> = FS[number] extends (this: infer C, ..._: unknown[]) => unknown ? C : never;
 
+export function bundle<as extends ((this: undefined, b: unknown) => unknown)[]>(...as: as): (...bs: Functions2Parameters<as>) => Functions2Returns<as>;
+export function bundle<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, ...bs: Functions2Parameters<as>) => Functions2Returns<as>;
 export function bundle<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, ...bs: Functions2Parameters<as>) => Functions2Returns<as> {
   return function (...bs) {
     return as.map((f, i) => f.call(this, bs[i])) as any;
   };
 }
 
+export function aggregate<as extends ((this: undefined) => unknown)[]>(...as: as): () => Functions2Returns<as>;
 export function aggregate<as extends (() => unknown)[]>(...as: as): (this: Functions2Context<as>) => Functions2Returns<as>;
+export function aggregate<as extends ((this: undefined, b: unknown) => unknown)[]>(...as: as): (b: Parameters<as[0]>[0]) => Functions2Returns<as>;
 export function aggregate<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Parameters<as[0]>[0]) => Functions2Returns<as>;
 export function aggregate<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Parameters<as[0]>[0]) => Functions2Returns<as> {
   return function (b) {
