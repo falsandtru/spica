dbcfb8b58abf018e194d6da7f74256424ef3094b
falsandtru <falsandtru@users.noreply.github.com>
2021-11-23 09:18:45 +0900

    Refactoring

--- src/arrow.ts
+++ src/arrow.ts
@@ -1,6 +1,9 @@
+import { Narrow, Intersect } from './type';
+
 type Functions2Parameters<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? Parameters<FS[P]>[0] : never; };
 type Functions2Returns<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? ReturnType<FS[P]> : never; };
 type Functions2Context<FS extends readonly ((..._: unknown[]) => unknown)[]> = FS[number] extends (this: infer C, ..._: unknown[]) => unknown ? C : never;
+type Functions2Parameters2<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? FS[P] extends (_?: undefined) => unknown ? never : Parameters<FS[P]>[0] : never; };
 
 export function bundle<as extends ((this: undefined, b: unknown) => unknown)[]>(...as: as): (...bs: Functions2Parameters<as>) => Functions2Returns<as>;
 export function bundle<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, ...bs: Functions2Parameters<as>) => Functions2Returns<as>;
@@ -12,9 +15,9 @@ export function bundle<as extends ((b: unknown) => unknown)[]>(...as: as): (this
 
 export function aggregate<as extends ((this: undefined) => unknown)[]>(...as: as): () => Functions2Returns<as>;
 export function aggregate<as extends (() => unknown)[]>(...as: as): (this: Functions2Context<as>) => Functions2Returns<as>;
-export function aggregate<as extends ((this: undefined, b: unknown) => unknown)[]>(...as: as): (b: Parameters<as[0]>[0]) => Functions2Returns<as>;
-export function aggregate<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Parameters<as[0]>[0]) => Functions2Returns<as>;
-export function aggregate<as extends ((b: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Parameters<as[0]>[0]) => Functions2Returns<as> {
+export function aggregate<as extends ((this: undefined, b?: unknown) => unknown)[]>(...as: as): (b: Intersect<Narrow<Functions2Parameters2<as>>>) => Functions2Returns<as>;
+export function aggregate<as extends ((b?: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Intersect<Narrow<Functions2Parameters2<as>>>) => Functions2Returns<as>;
+export function aggregate<as extends ((b?: unknown) => unknown)[]>(...as: as): (this: Functions2Context<as>, b: Intersect<Narrow<Functions2Parameters2<as>>>) => Functions2Returns<as> {
   return function (b) {
     return as.map(f => f.call(this, b)) as any;
   };
