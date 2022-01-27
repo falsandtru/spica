5ed370ddbe2a76e0718e65b70c9698b0047aefcf
falsandtru <falsandtru@users.noreply.github.com>
2022-01-28 01:03:40 +0900

    Refactoring

--- src/cache.ts
+++ src/cache.ts
@@ -292,12 +292,12 @@ export class Cache<K, V = undefined> implements IterableCollection<K, V> {
     const window = capacity;
     LRU[0] + LFU[0] === window && this.stats.slide();
     if ((LRU[0] + LFU[0]) % frequency || LRU[1] + LFU[1] === 0) return;
-    const rateR = rate(window, LRU[0], LRU[0] + LFU[0], LRU[1], LRU[1] + LFU[1]) * (50 + ratio) / 100;
-    const rateF = rate(window, LFU[0], LRU[0] + LFU[0], LFU[1], LRU[1] + LFU[1]) * (150 - ratio) / 100;
+    const rateR = rate(window, LRU[0], LRU[0] + LFU[0], LRU[1], LRU[1] + LFU[1]) * (50 + ratio);
+    const rateF = rate(window, LFU[0], LRU[0] + LFU[0], LFU[1], LRU[1] + LFU[1]) * (150 - ratio);
     // 操作頻度を超えてキャッシュ比率を増減させても余剰比率の消化が追いつかず無駄
     // LRUの下限設定ではLRU拡大の要否を迅速に判定できないためLFUのヒット率低下の検出で代替する
     if (ratio > 0 && rateR > rateF ||
-        ratio > 0 && rateF * 3 < rate(window, LFU[1], LRU[1] + LFU[1], LFU[0], LRU[0] + LFU[0]) * (150 - ratio) / 100) {
+        ratio > 0 && rateF * 3 < rate(window, LFU[1], LRU[1] + LFU[1], LFU[0], LRU[0] + LFU[0]) * (150 - ratio)) {
       if (indexes.LRU.length >= capacity * (100 - ratio) / 100) {
         //ratio % 10 || ratio === 100 || console.debug('-', this.ratio, LRU, LFU);
         --this.ratio;
