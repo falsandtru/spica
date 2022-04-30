b68dbdf4cd4110723810c02dbd3f672cbfb985db
falsandtru <falsandtru@users.noreply.github.com>
2022-05-01 02:38:07 +0900

    Refactoring

--- src/supervisor.test.ts
+++ src/supervisor.test.ts
@@ -3,7 +3,6 @@ import { Coroutine } from './coroutine';
 import { tick } from './clock';
 import { wait } from './timer';
 import { never } from './promise';
-import { Sequence } from './sequence';
 
 describe('Unit: lib/supervisor', function () {
   describe('Supervisor', function () {
@@ -466,19 +465,6 @@ describe('Unit: lib/supervisor', function () {
       sv.call(ns => ns, 0, (_, r) => assert(r === 4));
       sv.call(ns => ns, 0, (_, r) => assert(r === 6));
       sv.call(ns => ns, 0, (_, r) => void assert(r === 5) || done());
-    });
-
-    it('select', function (done) {
-      let cnt = 0;
-      const sv = new class TestSupervisor extends Supervisor<string, number, number, number> { }({
-      });
-      sv.register('0', (_, s) => [s + ++cnt, 0], 0);
-      sv.register('1', (_, s) => [s + ++cnt, 0], 1);
-      sv.register('2', (_, s) => [s + ++cnt, 0], 2);
-      sv.call(ns => Sequence.from(ns).filter(n => n === '2'), 0, (_, r) => assert(r === 3));
-      sv.call(ns => Sequence.from(ns).filter(n => n === '1'), 0, (_, r) => assert(r === 5));
-      sv.call(ns => Sequence.from(ns).filter(n => n === '0'), 0, (_, r) => assert(r === 6));
-      sv.call(ns => ns, 0, (_, r) => void assert(r === 4) || done());
     });
 
     it('kill', function (done) {
