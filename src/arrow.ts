7a3384f708dd1ee8002fe04d21382dcd30af64c2
falsandtru <falsandtru@users.noreply.github.com>
2022-05-01 09:04:53 +0900

    Refactoring

--- src/arrow.ts
+++ src/arrow.ts
@@ -1,34 +1,34 @@
 import type { Narrow, Intersect } from './type';
 import { singleton } from './function';
 
-type Functions2Parameters<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? Parameters<FS[P]>[0] : never; };
-type Functions2Returns<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? ReturnType<FS[P]> : never; };
-type Functions2Context<FS extends readonly ((..._: unknown[]) => unknown)[]> = FS[number] extends (this: infer C, ..._: unknown[]) => unknown ? C : never;
-type Functions2Parameters2<FS extends readonly ((..._: unknown[]) => unknown)[]> = { [P in keyof FS]: FS[P] extends FS[number] ? FS[P] extends (_?: undefined) => unknown ? never : Parameters<FS[P]>[0] : never; };
+type Function = (..._: unknown[]) => unknown;
+type ParamNs<FS extends readonly Function[], I extends number, F extends Function = never> = { [P in keyof FS]: FS[P] extends FS[number] ? Parameters<Exclude<FS[P], F>>[I] : FS[P]; };
+type Returns<FS extends readonly Function[]> = { [P in keyof FS]: FS[P] extends FS[number] ? ReturnType<FS[P]> : FS[P]; };
+type Context<FS extends readonly Function[]> = Intersect<Narrow<{ [P in keyof FS]: FS[P] extends (this: infer C, ..._: unknown[]) => unknown ? C : never; }>>;
 
-export function bundle<fs extends ((this: undefined, a: unknown) => unknown)[]>(...fs: fs): (...bs: Functions2Parameters<fs>) => Functions2Returns<fs>;
-export function bundle<fs extends ((a: unknown) => unknown)[]>(...fs: fs): (this: Functions2Context<fs>, ...bs: Functions2Parameters<fs>) => Functions2Returns<fs>;
-export function bundle<fs extends ((a: unknown) => unknown)[]>(...fs: fs): (this: Functions2Context<fs>, ...bs: Functions2Parameters<fs>) => Functions2Returns<fs> {
+export function bundle<fs extends ((this: undefined, a: unknown) => unknown)[]>(...fs: fs): (...bs: ParamNs<fs, 0>) => Returns<fs>;
+export function bundle<fs extends ((a: unknown) => unknown)[]>(...fs: fs): (this: Context<fs>, ...bs: ParamNs<fs, 0>) => Returns<fs>;
+export function bundle<fs extends ((a: unknown) => unknown)[]>(...fs: fs): (this: Context<fs>, ...bs: ParamNs<fs, 0>) => Returns<fs> {
   return function (...bs) {
-    return fs.map((f, i) => f.call(this, bs[i])) as any;
+    return fs.map((f, i) => f.call(this, bs[i])) as Returns<fs>;
   };
 }
 
-export function aggregate<fs extends ((this: undefined) => unknown)[]>(...fs: fs): () => Functions2Returns<fs>;
-export function aggregate<fs extends (() => unknown)[]>(...fs: fs): (this: Functions2Context<fs>) => Functions2Returns<fs>;
-export function aggregate<fs extends ((this: undefined, a?: unknown) => unknown)[]>(...fs: fs): (a: Intersect<Narrow<Functions2Parameters2<fs>>>) => Functions2Returns<fs>;
-export function aggregate<fs extends ((a?: unknown) => unknown)[]>(...fs: fs): (this: Functions2Context<fs>, a: Intersect<Narrow<Functions2Parameters2<fs>>>) => Functions2Returns<fs>;
-export function aggregate<fs extends ((a?: unknown) => unknown)[]>(...fs: fs): (this: Functions2Context<fs>, a: Intersect<Narrow<Functions2Parameters2<fs>>>) => Functions2Returns<fs> {
+export function aggregate<fs extends ((this: undefined) => unknown)[]>(...fs: fs): () => Returns<fs>;
+export function aggregate<fs extends (() => unknown)[]>(...fs: fs): (this: Context<fs>) => Returns<fs>;
+export function aggregate<fs extends ((this: undefined, a?: unknown) => unknown)[]>(...fs: fs): (a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => Returns<fs>;
+export function aggregate<fs extends ((a?: unknown) => unknown)[]>(...fs: fs): (this: Context<fs>, a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => Returns<fs>;
+export function aggregate<fs extends ((a?: unknown) => unknown)[]>(...fs: fs): (this: Context<fs>, a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => Returns<fs> {
   return function (a) {
-    return fs.map(f => f.call(this, a)) as any;
+    return fs.map(f => f.call(this, a)) as Returns<fs>;
   };
 }
 
 export function assemble<fs extends ((this: undefined) => () => void)[]>(...fs: fs): () => () => undefined;
-export function assemble<fs extends (() => () => void)[]>(...fs: fs): (this: Functions2Context<fs>) => () => undefined;
-export function assemble<fs extends ((this: undefined, a?: unknown) => () => void)[]>(...fs: fs): (a: Intersect<Narrow<Functions2Parameters2<fs>>>) => () => undefined;
-export function assemble<fs extends ((a?: unknown) => () => void)[]>(...fs: fs): (this: Functions2Context<fs>, a: Intersect<Narrow<Functions2Parameters2<fs>>>) => () => undefined;
-export function assemble<fs extends ((a?: unknown) => () => void)[]>(...fs: fs): (this: Functions2Context<fs>, a: Intersect<Narrow<Functions2Parameters2<fs>>>) => () => undefined {
+export function assemble<fs extends (() => () => void)[]>(...fs: fs): (this: Context<fs>) => () => undefined;
+export function assemble<fs extends ((this: undefined, a?: unknown) => () => void)[]>(...fs: fs): (a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => () => undefined;
+export function assemble<fs extends ((a?: unknown) => () => void)[]>(...fs: fs): (this: Context<fs>, a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => () => undefined;
+export function assemble<fs extends ((a?: unknown) => () => void)[]>(...fs: fs): (this: Context<fs>, a: Intersect<Narrow<ParamNs<fs, 0, () => unknown>>>) => () => undefined {
   return function (a) {
     const gs: (() => void)[] = [];
     try {
