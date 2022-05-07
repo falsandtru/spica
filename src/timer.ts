e881f7767a5e983d33b482f016fb9b5b4cb8c9f8
falsandtru <falsandtru@users.noreply.github.com>
2022-05-07 13:51:12 +0900

    Refactoring

--- src/timer.ts
+++ src/timer.ts
@@ -1,13 +1,16 @@
 import { global } from './global';
 import { singleton } from './function';
 
-export const setImmediate = template(global.setImmediate, global.clearImmediate);
 export const setTimeout = template(global.setTimeout, global.clearTimeout);
 export const setInterval = template(global.setInterval, global.clearInterval);
 
-function template(registrator: Function, unregistrator: Function) {
-  return <T extends readonly unknown[]>(handler: (...args: T) => void, timeout?: number, ...args: T): () => void => {
-    const timer = registrator(handler, timeout, ...args);
-    return singleton(() => void unregistrator(timer));
+function template(set: Function, unset: Function) {
+  return <a>(wait: number, handler: () => a, unhandler?: (a: a) => void): () => void => {
+    let params: [a];
+    const timer = set(() => params = [handler()], wait);
+    return singleton(() => {
+      unset(timer);
+      params && unhandler?.(params[0]);
+    });
   };
 }
