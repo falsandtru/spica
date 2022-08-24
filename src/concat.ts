100ea80c0754d85ab0fd44902dbf296b5c05fb5e
falsandtru <falsandtru@users.noreply.github.com>
2022-08-25 02:25:51 +0900

    Refactoring

--- src/concat.ts
+++ src/concat.ts
@@ -1,7 +1,5 @@
-import { isArray } from './alias';
-
-export function concat<T>(target: T[], source: Iterable<T>): T[] {
-  if (isArray(source)) {
+export function concat<T>(target: T[], source: Iterable<T> | ArrayLike<T>): T[] {
+  if ('length' in source) {
     for (let i = 0; i < source.length; ++i) {
       target.push(source[i]);
     }
