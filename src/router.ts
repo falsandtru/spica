61a9b6ce286ce89dc6cffae661d72411e0a0fed2
falsandtru <falsandtru@users.noreply.github.com>
2022-08-25 15:20:08 +0900

    Refactoring

--- src/router.ts
+++ src/router.ts
@@ -9,7 +9,8 @@ export function router<T>(config: Record<string, (path: string) => T>): (path: s
   for (const pattern of patterns) {
     if (pattern[0] !== '/') throw new Error(`Spica: Router: Pattern must start with "/": ${pattern}`);
     if (/\s/.test(pattern)) throw new Error(`Spica: Router: Pattern must not have whitespace: ${pattern}`);
-    if (/\*\*|[\[\]]/.test(pattern)) throw new Error(`Spica: Router: Invalid pattern: ${pattern}`);
+    // Prohibit unimplemented patterns.
+    if (pattern.includes('**')) throw new Error(`Spica: Router: Invalid pattern: ${pattern}`);
   }
   return (path: string) => {
     const pathname = path.slice(0, path.search(/[?#]|$/));
@@ -44,7 +45,7 @@ export namespace router {
         parse(pattern)
           .map(token =>
             token[0] + token.slice(-1) === '{}'
-              ? split(token.slice(1, -1)).flatMap(expand)
+              ? separate(token.slice(1, -1)).flatMap(expand)
               : [token]))
         .mapM(Sequence.from)
         .extract()
@@ -52,43 +53,60 @@ export namespace router {
     });
     function parse(pattern: string): string[] {
       const results: string[] = [];
-      const stack: '{'[] = [];
+      const stack: ('[' | '(' | '{')[] = [];
+      const mirror = {
+        ']': '[',
+        ')': '(',
+      } as const;
       let buffer = '';
-      for (const token of pattern.match(/[{}]|[^{}]+|$/g) ?? []) {
+      for (const token of pattern.match(/\\.?|[\[\](){}]|[^\\\[\](){}]+|$/g) ?? []) {
         switch (token) {
           case '':
-            flush(true);
-            continue;
-          case ',':
-            stack.length === 0
-              ? flush()
-              : buffer += token;
+            flush();
             continue;
           case '{':
             stack.length === 0 && flush();
             buffer += token;
-            stack.unshift(token);
+            stack[0] !== '[' && stack.unshift(token);
             continue;
           case '}':
             stack[0] === '{' && stack.shift();
             buffer += token;
             stack.length === 0 && flush();
             continue;
+          case '(':
+          case '[':
+            // Prohibit unimplemented patterns.
+            if (1) throw new Error(`Spica: Router: Invalid pattern: ${pattern}`);
+            buffer += token;
+            stack[0] !== '[' && stack.unshift(token);
+            continue;
+          case ')':
+          case ']':
+            stack[0] === mirror[token] && stack.shift();
+            buffer += token;
+            continue;
         }
         buffer += token;
       }
+      results.length === 0 && results.push('');
       return results;
 
-      function flush(force = false): void {
-        (buffer || force) && results.push(buffer);
+      function flush(): void {
+        buffer && results.push(buffer);
         buffer = '';
       }
     }
-    function split(pattern: string): string[] {
+    function separate(pattern: string): string[] {
       const results: string[] = [];
-      const stack: '{'[] = [];
+      const stack: ('[' | '(' | '{')[] = [];
+      const mirror = {
+        ']': '[',
+        ')': '(',
+        '}': '{',
+      } as const;
       let buffer = '';
-      for (const token of pattern.match(/[,{}]|[^,{}]+|$/g) ?? []) {
+      for (const token of pattern.match(/\\.?|[,\[\](){}]|[^\\,\[\](){}]+|$/g) ?? []) {
         switch (token) {
           case '':
             flush();
@@ -98,12 +116,16 @@ export namespace router {
               ? flush()
               : buffer += token;
             continue;
+          case '[':
+          case '(':
           case '{':
             buffer += token;
-            stack.unshift(token);
+            stack[0] !== '[' && stack.unshift(token);
             continue;
+          case ']':
+          case ')':
           case '}':
-            stack[0] === '{' && stack.shift();
+            stack[0] === mirror[token] && stack.shift();
             buffer += token;
             continue;
         }
@@ -120,13 +142,13 @@ export namespace router {
     function match(pattern: string, segment: string): boolean {
       assert(segment === '/' || !segment.startsWith('/'));
       if (segment[0] === '.' && ['?', '*'].includes(pattern[0])) return false;
-      return match$([...optimize(pattern)], 0, [...segment], 0);
+      return match$(split(optimize(pattern)), 0, segment, 0);
     }
 
-    function match$(ps: readonly string[], i: number, ss: readonly string[], j: number): boolean {
-      for (; i < ps.length || 1; ++i, ++j) {
+    function match$(ps: readonly string[], i: number, segment: string, j: number): boolean {
+      for (; i < ps.length || 1; ++i) {
         const p = ps[i] ?? '';
-        const s = ss[j] ?? '';
+        const s = segment.slice(j);
         switch (p) {
           case '':
             return s === '';
@@ -136,26 +158,73 @@ export namespace router {
               case '/':
                 return false;
               default:
+                ++j;
                 continue;
             }
           case '*':
             switch (s) {
               case '':
               case '/':
-                --j;
                 continue;
             }
-            for (let k = ss.length; k >= j; --k) {
-              if (match$(ps, i + 1, ss, k)) return true;
+            for (let k = segment.length; k >= j; --k) {
+              if (match$(ps, i + 1, segment, k)) return true;
             }
             return false;
           default:
-            if (s === p) continue;
-            return false;
+            if (s.slice(0, p.length) !== p) return false;
+            j += p.length;
+            continue;
         }
       }
       return true;
     }
+    function split(pattern: string): string[] {
+      const results: string[] = [];
+      const stack: ('[' | '(' | '{')[] = [];
+      const mirror = {
+        ']': '[',
+        ')': '(',
+        '}': '{',
+      } as const;
+      let buffer = '';
+      for (const token of pattern.match(/\\.?|[*?\[\](){}]|[^\\*?\[\](){}]+|$/g) ?? []) {
+        switch (token) {
+          case '':
+            flush();
+            continue;
+          case '*':
+          case '?':
+            stack.length === 0 && flush();
+            buffer += token;
+            stack.length === 0 && flush();
+            continue;
+          case '{':
+          case '[':
+          case '(':
+            buffer += token;
+            stack[0] !== '[' && stack.unshift(token);
+            continue;
+          // @ts-expect-error
+          case '}':
+            if (stack[0] === '{') throw new Error(`Spica: Router: Invalid pattern: ${pattern}`);
+          case ']':
+          case ')':
+            stack[0] === mirror[token] && stack.shift();
+            buffer += token;
+            continue;
+        }
+        buffer += token;
+      }
+      return results;
+
+      function flush(): void {
+        buffer && results.push(buffer);
+        buffer = '';
+      }
+    }
+    const optimize = memoize(fix((pattern: string) =>
+      pattern.replace(/(\*)\*|\*(\?+)\*?(?!\*)/g, '$1$2*')));
 
     return {
       compare,
@@ -164,6 +233,3 @@ export namespace router {
     };
   }
 }
-
-const optimize = fix((pattern: string) =>
-  pattern.replace(/(\*)\*|\*(\?+)\*?(?!\*)/g, '$1$2*'));
