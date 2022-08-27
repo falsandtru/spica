56899778b6fb05ce3becfd16023058b4534004cc
falsandtru <falsandtru@users.noreply.github.com>
2022-08-28 06:13:02 +0900

    Refactoring

--- benchmark/array.ts
+++ benchmark/array.ts
@@ -7,17 +7,17 @@ describe('Benchmark:', function () {
   describe('indexOf', function () {
     it('10', function (done) {
       const r = `${rnd0Z(9)}-`.split('');
-      benchmark('array indexOf 10', () => r.indexOf('-'), done);
+      benchmark('Array indexOf 10', () => r.indexOf('-'), done);
     });
 
     it('100', function (done) {
       const r = `${rnd0Z(99)}-`.split('');
-      benchmark('array indexOf 100', () => r.indexOf('-'), done);
+      benchmark('Array indexOf 100', () => r.indexOf('-'), done);
     });
 
     it('1000', function (done) {
       const r = `${rnd0Z(999)}-`.split('');
-      benchmark('array indexOf 1000', () => r.indexOf('-'), done);
+      benchmark('Array indexOf 1000', () => r.indexOf('-'), done);
     });
 
   });
@@ -33,15 +33,15 @@ describe('Benchmark:', function () {
     }
 
     it('1', function (done) {
-      benchmark('array push for-of 1', f(Array(1).fill(1)), done);
+      benchmark('Array push for-of 1', f(Array(1).fill(1)), done);
     });
 
     it('10', function (done) {
-      benchmark('array push for-of 10', f(Array(10).fill(1)), done);
+      benchmark('Array push for-of 10', f(Array(10).fill(1)), done);
     });
 
     it('100', function (done) {
-      benchmark('array push for-of 100', f(Array(100).fill(1)), done);
+      benchmark('Array push for-of 100', f(Array(100).fill(1)), done);
     });
 
   });
@@ -55,15 +55,15 @@ describe('Benchmark:', function () {
     }
 
     it('1', function (done) {
-      benchmark('array push spread 1', f(Array(1).fill(1)), done);
+      benchmark('Array push spread 1', f(Array(1).fill(1)), done);
     });
 
     it('10', function (done) {
-      benchmark('array push spread 10', f(Array(10).fill(1)), done);
+      benchmark('Array push spread 10', f(Array(10).fill(1)), done);
     });
 
     it('100', function (done) {
-      benchmark('array push spread 100', f(Array(100).fill(1)), done);
+      benchmark('Array push spread 100', f(Array(100).fill(1)), done);
     });
 
   });
---
+++ benchmark/data.ts
@@ -0,0 +1,55 @@
+import { benchmark } from './benchmark';
+import { Stack } from '../src/stack';
+import { Queue } from '../src/queue';
+
+describe('Benchmark:', function () {
+  this.timeout(10 * 1e3);
+
+  describe('data', function () {
+    afterEach(done => {
+      setTimeout(done, 1000);
+    });
+
+    it('Stack new', function (done) {
+      benchmark('Stack new', () => new Stack(), done);
+    });
+
+    it('Queue new', function (done) {
+      benchmark('Queue new', () => new Queue(), done);
+    });
+
+    for (const length of [1, 10, 100, 1000, 10000, 100000, 1000000]) {
+      it(`Array first ${length.toLocaleString('en')}`, function (done) {
+        const data: unknown[] = [];
+        for (let i = 0; i < length; ++i) data.push(0);
+        benchmark(`Array first ${length.toLocaleString('en')}`, () => (data.shift(), data.unshift(0)), done);
+      });
+
+      it(`Array stack ${length.toLocaleString('en')}`, function (done) {
+        const data: unknown[] = [];
+        for (let i = 0; i < length; ++i) data.push(0);
+        benchmark(`Array stack ${length.toLocaleString('en')}`, () => (data.pop(), data.push(0)), done);
+      });
+
+      it(`Array queue ${length.toLocaleString('en')}`, function (done) {
+        const data: unknown[] = [];
+        for (let i = 0; i < length; ++i) data.push(0);
+        benchmark(`Array queue ${length.toLocaleString('en')}`, () => (data.shift(), data.push(0)), done);
+      });
+
+      it(`Stack ${length.toLocaleString('en')}`, function (done) {
+        const data = new Stack();
+        for (let i = 0; i < length; ++i) data.push(0);
+        benchmark(`Stack ${length.toLocaleString('en')}`, () => (data.pop(), data.push(0)), done);
+      });
+
+      it(`Queue ${length.toLocaleString('en')}`, function (done) {
+        const data = new Queue();
+        for (let i = 0; i < length; ++i) data.enqueue(0);
+        benchmark(`Queue ${length.toLocaleString('en')}`, () => (data.dequeue(), data.enqueue(0)), done);
+      });
+    }
+
+  });
+
+});
