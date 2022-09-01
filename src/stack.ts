27698e9df24c4a24839b84fd26e6f5de78df7cdf
falsandtru <falsandtru@users.noreply.github.com>
2022-09-01 22:53:11 +0900

    Refactoring

--- src/stack.ts
+++ src/stack.ts
@@ -1,85 +1,40 @@
 import { Array } from './global';
 
-const capacity = 2048;
-const initcap = 16;
+const size = 16;
 
 export class Stack<T> {
-  private head = new FixedStack<T>(Array(initcap));
-  private tail = this.head;
-  private count = 0;
+  private array = Array<T | undefined>(size);
+  private index = 0;
   public get length(): number {
-    return this.count === 0
-      ? this.head.length
-      : this.head.length + this.tail.length + capacity * (this.count - 1);
+    return this.index;
   }
   public isEmpty(): boolean {
-    return this.head.isEmpty();
+    return this.index === 0;
   }
   public push(value: T): void {
-    const tail = this.tail;
-    if (tail.isFull()) {
-      this.tail = tail.next ??= new FixedStack<T>(Array(capacity), tail);
-      ++this.count;
-    }
-    this.tail.push(value);
+    this.array[this.index++] = value;
   }
   public pop(): T | undefined {
-    const tail = this.tail;
-    const value = tail.pop();
-    if (tail.isEmpty() && tail.prev) {
-      --this.count;
-      this.tail = tail.prev;
-    }
+    if (this.index === 0) return;
+    const array = this.array;
+    const i = --this.index;
+    const value = array[i];
+    array[i] = void 0;
     return value;
   }
   public peek(index: 0 | -1 = 0): T | undefined {
     return index === 0
-      ? this.tail.peek(0)
-      : this.head.peek(-1);
+      ? this.array[(this.index || 1) - 1]
+      : this.array[0];
   }
   public clear(): void {
-    this.head = this.tail = new FixedStack(Array(initcap));
-    this.count = 0;
+    this.array = Array(size);
+    this.index = 0;
   }
   public *[Symbol.iterator](): Iterator<T, undefined, undefined> {
     while (!this.isEmpty()) {
       yield this.pop()!;
     }
     return;
-  }
-}
-
-class FixedStack<T> {
-  constructor(
-    private array: Array<T | undefined>,
-    public prev?: FixedStack<T>,
-  ) {
-    assert((this.array.length & this.array.length - 1) === 0);
-  }
-  private readonly mask = this.array.length - 1;
-  private tail = 0;
-  public next?: FixedStack<T>;
-  public get length(): number {
-    return this.tail;
-  }
-  public isEmpty(): boolean {
-    return this.tail === 0;
-  }
-  public isFull(): boolean {
-    return this.tail === this.array.length;
-  }
-  public push(value: T): void {
-    this.array[this.tail++] = value;
-  }
-  public pop(): T | undefined {
-    if (this.isEmpty()) return;
-    const value = this.array[--this.tail];
-    this.array[this.tail] = void 0;
-    return value;
-  }
-  public peek(index: 0 | -1 = 0): T | undefined {
-    return index === 0
-      ? this.array[this.tail - 1 & this.mask]
-      : this.array[0];
   }
 }
