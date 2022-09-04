995acc7a56323b72b01a4ad64f45d3f2d4f36560
falsandtru <falsandtru@users.noreply.github.com>
2022-09-05 01:38:18 +0900

    Refactoring

--- benchmark/list.ts
+++ benchmark/list.ts
@@ -7,51 +7,44 @@ describe('Benchmark:', function () {
   this.timeout(10 * 1e3);
 
   describe('List', function () {
-    it('new', function (done) {
+    it('Yallist new', function (done) {
       benchmark('Yallist new', () => Yallist.create(), done);
     });
 
-    it('new', function (done) {
+    it('InvList new', function (done) {
       benchmark('InvList new', () => new InvList(), done);
     });
 
-    it('new', function (done) {
+    it('IxList  new', function (done) {
       benchmark('IxList  new', () => new IxList(), done);
     });
 
     for (const length of [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]) {
       it(`Yallist add ${length.toLocaleString('en')}`, function (done) {
-        const list = new Yallist();
-        for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`Yallist add ${length.toLocaleString('en')}`, () => {
-          list.pop();
-          list.unshift(0);
+          const list = new Yallist();
+          for (let i = 0; i < length; ++i) list.push(0);
         }, done);
       });
 
       it(`InvList add ${length.toLocaleString('en')}`, function (done) {
-        const list = new InvList();
-        for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`InvList add ${length.toLocaleString('en')}`, () => {
-          list.pop();
-          list.unshift(0);
+          const list = new InvList();
+          for (let i = 0; i < length; ++i) list.push(0);
         }, done);
       });
 
       it(`InvList add ${length.toLocaleString('en')} rotationally`, function (done) {
-        const list = new InvList();
-        for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`InvList add ${length.toLocaleString('en')} rotationally`, () => {
-          list.unshiftRotationally(0);
+          const list = new InvList();
+          for (let i = 0; i < length; ++i) list.unshiftRotationally(0);
         }, done);
       });
 
       it(`IxList  add ${length.toLocaleString('en')}`, function (done) {
-        const list = new IxList();
-        for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`IxList  add ${length.toLocaleString('en')}`, () => {
-          list.pop();
-          list.unshift(0);
+          const list = new IxList();
+          for (let i = 0; i < length; ++i) list.push(0);
         }, done);
       });
 
@@ -59,7 +52,7 @@ describe('Benchmark:', function () {
         const list = new IxList();
         for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`IxList  add ${length.toLocaleString('en')} rotationally`, () => {
-          list.unshiftRotationally(0);
+          for (let i = 0; i < length; ++i) list.unshiftRotationally(0);
         }, done);
       });
 
@@ -67,7 +60,7 @@ describe('Benchmark:', function () {
         const list = new IxList(length);
         for (let i = 0; i < length; ++i) list.push(0);
         benchmark(`IxList  add ${length.toLocaleString('en')} with constraint`, () => {
-          list.unshift(0);
+          for (let i = 0; i < length; ++i) list.unshift(0);
         }, done);
       });
     }
