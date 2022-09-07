91b1a33a04dbc9153501658bf35bdd4641320d54
falsandtru <falsandtru@users.noreply.github.com>
2022-09-08 00:27:21 +0900

    Refactoring

--- benchmark/memoize.ts
+++ benchmark/memoize.ts
@@ -4,26 +4,28 @@ import { Cache } from '../src/cache';
 
 describe('Benchmark:', function () {
   describe('memoize', function () {
-    const size = 2 << 14;
-    const mask = size - 1;
+    for (const exp of [10, 12, 14, 16, 18, 20]) {
+      const size = 1 << exp;
+      const mask = size - 1;
 
-    it('Map', function (done) {
-      const f = memoize(a => a, new Map());
-      let i = 0;
-      benchmark('memoize Map', () => f(i = ++i & mask), done);
-    });
+      it(`Map   ${size.toLocaleString('en')}`, function (done) {
+        const f = memoize(a => a, new Map());
+        let i = 0;
+        benchmark(`memoize Map   ${size.toLocaleString('en')}`, () => f(i = ++i & mask), done);
+      });
 
-    it('Array', function (done) {
-      const f = memoize(a => a, []);
-      let i = 0;
-      benchmark('memoize Array', () => f(i = ++i & mask), done);
-    });
+      it(`Array ${size.toLocaleString('en')}`, function (done) {
+        const f = memoize(a => a, []);
+        let i = 0;
+        benchmark(`memoize Array ${size.toLocaleString('en')}`, () => f(i = ++i & mask), done);
+      });
 
-    it('Cache', function (done) {
-      const f = memoize(a => a, new Cache(size));
-      let i = 0;
-      benchmark('memoize Cache', () => f(i = ++i & mask), done);
-    });
+      it(`Cache ${size.toLocaleString('en')}`, function (done) {
+        const f = memoize(a => a, new Cache(size));
+        let i = 0;
+        benchmark(`memoize Cache ${size.toLocaleString('en')}`, () => f(i = ++i & mask), done);
+      });
+    }
   });
 
   describe('reduce', function () {
