4c563a5d621fa7f7e712b0c39994ce1271cf780b
falsandtru <falsandtru@users.noreply.github.com>
2022-09-08 05:39:05 +0900

    Refactoring

--- benchmark/heap.ts
+++ benchmark/heap.ts
@@ -1,10 +1,19 @@
 import { benchmark } from './benchmark';
-import { Heap } from '../src/heap';
+import { Heap, MultiHeap } from '../src/heap';
+import { Heap as ObjectHeap } from '../src/heap.uint';
 
 describe('Benchmark:', function () {
   describe('Heap', function () {
     it('Heap new', function (done) {
       benchmark('Heap new', () => new Heap(), done);
+    });
+
+    it('MultiHeap new', function (done) {
+      benchmark('MultiHeap new', () => new MultiHeap(), done);
+    });
+
+    it('ObjectHeap uint new', function (done) {
+      benchmark('ObjectHeap new', () => new ObjectHeap(), done);
     });
 
     for (const length of [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]) {
@@ -13,7 +22,23 @@ describe('Benchmark:', function () {
         for (let i = 0; i < length; ++i) heap.insert(1, i);
         let i = 0;
         benchmark(`Heap insert/extract ${length.toLocaleString('en')}`, () =>
-          heap.extract() && heap.insert(1, i++ % length), done);
+          heap.extract() && heap.insert(1, i = i++ % length), done);
+      });
+
+      it(`MultiHeap insert/extract ${length.toLocaleString('en')}`, function (done) {
+        const heap = new MultiHeap<number>(MultiHeap.min);
+        for (let i = 0; i < length; ++i) heap.insert(1, i);
+        let i = 0;
+        benchmark(`MultiHeap insert/extract ${length.toLocaleString('en')}`, () =>
+          heap.extract() && heap.insert(1, i = i++ % length), done);
+      });
+
+      it(`ObjectHeap insert/extract ${length.toLocaleString('en')}`, function (done) {
+        const heap = new ObjectHeap<number>();
+        for (let i = 0; i < length; ++i) heap.insert(1, i);
+        let i = 0;
+        benchmark(`ObjectHeap insert/extract ${length.toLocaleString('en')}`, () =>
+          heap.extract() && heap.insert(1, i = i++ % length), done);
       });
     }
 
