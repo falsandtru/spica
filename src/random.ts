7e2be34b07db6f52dc8369c612261b674462c6de
falsandtru <falsandtru@users.noreply.github.com>
2022-09-12 11:37:26 +0900

    Refactoring

--- src/random.ts
+++ src/random.ts
@@ -1,19 +1,20 @@
 import { Set, crypto } from './global';
 
-const bases = [...Array(7)].map((_, i) => 1 << i);
-assert.deepStrictEqual(bases, [1, 2, 4, 8, 16, 32, 64]);
-const dict0_ = [
+const radixes = Object.freeze([...Array(7)].map((_, i) => 1 << i));
+assert.deepStrictEqual(radixes, [1, 2, 4, 8, 16, 32, 64]);
+const masks = Object.freeze(radixes.map(radix => radix - 1));
+const dict0_ = Object.freeze([
   ...[...Array(36)].map((_, i) => i.toString(36)),
   ...[...Array(36)].map((_, i) => i.toString(36).toUpperCase()).slice(-26),
   '-', '_',
-];
+]);
 assert(dict0_.length === 64);
 // eslint-disable-next-line
 assert(dict0_.join('').match(/^0.*9a.*zA.*Z-_$/));
-const dictAz = [
+const dictAz = Object.freeze([
   ...[...Array(36)].map((_, i) => i.toString(36).toUpperCase()).slice(-26),
   ...[...Array(36)].map((_, i) => i.toString(36)).slice(-26),
-];
+]);
 assert(dictAz.length === 52);
 // eslint-disable-next-line
 assert(dictAz.join('').match(/^A.*Za.*z$/));
@@ -63,20 +64,19 @@ export function unique(rnd: (len: number) => string, len: number): () => string 
   };
 }
 
-function cons(radix: number): () => number {
-  const base = bases.find(base => base >= radix)!;
-  assert(base);
-  const len = bases.indexOf(base) as 1;
+function cons(size: number): () => number {
+  const len = radixes.findIndex(radix => radix >= size) as 1;
+  assert(len > 0);
   return () => {
     while (true) {
       const r = random(len)
-      assert(r < base);
-      if (r < radix) return r;
+      assert(r < radixes[len]);
+      if (r < size) return r;
     }
   };
 }
 
-function conv(rnd: () => number, dict: string[]): (len?: number) => string {
+function conv(rnd: () => number, dict: readonly string[]): (len?: number) => string {
   return (len = 1) => {
     let acc = '';
     while (len--) {
@@ -88,7 +88,6 @@ function conv(rnd: () => number, dict: string[]): (len?: number) => string {
 
 const buffer = new Uint16Array(512);
 const digit = 16;
-const masks = bases.map((_, i) => +`0b${'1'.repeat(i) || 0}`);
 let index = buffer.length;
 let offset = digit;
 
