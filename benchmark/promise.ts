8a0c62fd0d043240504ccb45a4633141cc44ac4d
falsandtru <falsandtru@users.noreply.github.com>
2022-09-18 22:41:43 +0900

    Refactoring

--- benchmark/promise.ts
+++ benchmark/promise.ts
@@ -1,7 +1,8 @@
 import { benchmark } from './benchmark';
 import { Promise } from '../src/global';
-import { AtomicPromise } from '../src/promise';
 import Bluebird from 'bluebird';
+import { AtomicPromise } from '../src/promise';
+import { AtomicFuture, Future } from '../src/future';
 import { clock } from '../src/clock';
 import { noop } from '../src/function';
 
@@ -18,88 +19,134 @@ describe('Benchmark:', function () {
   }
 
   describe('Promise', function () {
-    it('resolve', function (done) {
+    it('Promise  resolve', function (done) {
       benchmark('Promise  resolve', () => void Promise.resolve(), done);
     });
 
-    it('resolve', function (done) {
-      benchmark('APromise resolve', () => void AtomicPromise.resolve(), done);
-    });
-
-    it('resolve', function (done) {
+    it('Bluebird resolve', function (done) {
       benchmark('Bluebird resolve', () => void Bluebird.resolve(), done);
     });
 
-    it('new', function (done) {
+    it('APromise resolve', function (done) {
+      benchmark('APromise resolve', () => void AtomicPromise.resolve(), done);
+    });
+
+    it('Promise  new', function (done) {
       benchmark('Promise  new', () => void new Promise(resolve => resolve(void 0)), done);
     });
 
-    it('new', function (done) {
-      benchmark('APromise new', () => void new AtomicPromise(resolve => resolve(void 0)), done);
-    });
-
-    it('new', function (done) {
+    it('Bluebird new', function (done) {
       benchmark('Bluebird new', () => void new Bluebird(resolve => resolve()), done);
     });
 
-    it('run', function (done) {
+    it('APromise new', function (done) {
+      benchmark('APromise new', () => void new AtomicPromise(resolve => resolve(void 0)), done);
+    });
+
+    it('Future   new', function (done) {
+      benchmark('Future   new', () => void new Future().bind(), done);
+    });
+
+    it('AFuture  new', function (done) {
+      benchmark('AFuture  new', () => void new AtomicFuture().bind(), done);
+    });
+
+    it('Promise  run', function (done) {
       benchmark('Promise  run', done => void new Promise(resolve => clock.then(resolve)).then(done), done, { defer: true });
     });
 
-    it('run', function (done) {
-      benchmark('APromise run', done => void new AtomicPromise(resolve => clock.then(resolve)).then(done), done, { defer: true });
-    });
-
-    it('run', function (done) {
+    it('Bluebird run', function (done) {
       benchmark('Bluebird run', done => void new Bluebird(resolve => clock.then(resolve)).then(done), done, { defer: true });
     });
 
-    it('then', function (done) {
+    it('APromise run', function (done) {
+      benchmark('APromise run', done => void new AtomicPromise(resolve => clock.then(resolve)).then(done), done, { defer: true });
+    });
+
+    it('Future   run', function (done) {
+      benchmark('Future   run', done => void new Future().bind(clock).then(done), done, { defer: true });
+    });
+
+    it('AFuture  run', function (done) {
+      benchmark('AFuture  run', done => void new AtomicFuture().bind(clock).then(done), done, { defer: true });
+    });
+
+    it('Promise  then', function (done) {
       const p = Promise.resolve();
       benchmark('Promise  then', done => void p.then(done), done, { defer: true });
     });
 
-    it('then', function (done) {
-      const p = AtomicPromise.resolve();
-      benchmark('APromise then', () => void p.then(noop), done);
-    });
-
-    it('then', function (done) {
+    it('Bluebird then', function (done) {
       const p = Bluebird.resolve();
       benchmark('Bluebird then', done => void p.then(done), done, { defer: true });
     });
 
-    it('chain 10', function (done) {
+    it('APromise then', function (done) {
+      const p = AtomicPromise.resolve();
+      benchmark('APromise then', () => void p.then(noop), done);
+    });
+
+    it('Future   then', function (done) {
+      const p = new Future().bind();
+      benchmark('Future   then', () => void p.then(noop), done);
+    });
+
+    it('AFuture  then', function (done) {
+      const p = new AtomicFuture().bind();
+      benchmark('AFuture  then', () => void p.then(noop), done);
+    });
+
+    it('Promise  chain 10', function (done) {
       const p = Promise.resolve();
       benchmark('Promise  chain 10', done => void chain(p, 9).then(done), done, { defer: true });
     });
 
-    it('chain 10', function (done) {
-      const p = AtomicPromise.resolve();
-      benchmark('APromise chain 10', () => void chain(p, 10), done);
-    });
-
-    it('chain 10', function (done) {
+    it('Bluebird chain 10', function (done) {
       const p = Bluebird.resolve();
       benchmark('Bluebird chain 10', done => void chain(p, 9).then(done), done, { defer: true });
     });
 
-    it('chain 100', function (done) {
+    it('APromise chain 10', function (done) {
+      const p = AtomicPromise.resolve();
+      benchmark('APromise chain 10', () => void chain(p, 10), done);
+    });
+
+    it('Future   chain 10', function (done) {
+      const p = new Future().bind();
+      benchmark('Future   chain 10', () => void chain(p, 10), done);
+    });
+
+    it('AFuture  chain 10', function (done) {
+      const p = new AtomicFuture().bind();
+      benchmark('AFuture  chain 10', () => void chain(p, 10), done);
+    });
+
+    it('Promise  chain 100', function (done) {
       const p = Promise.resolve();
       benchmark('Promise  chain 100', done => void chain(p, 99).then(done), done, { defer: true });
     });
 
-    it('chain 100', function (done) {
-      const p = AtomicPromise.resolve();
-      benchmark('APromise chain 100', () => void chain(p, 100), done);
-    });
-
-    it('chain 100', function (done) {
+    it('Bluebird chain 100', function (done) {
       const p = Bluebird.resolve();
       benchmark('Bluebird chain 100', done => void chain(p, 99).then(done), done, { defer: true });
     });
 
-    it('all 2', function (done) {
+    it('APromise chain 100', function (done) {
+      const p = AtomicPromise.resolve();
+      benchmark('APromise chain 100', () => void chain(p, 100), done);
+    });
+
+    it('Future   chain 100', function (done) {
+      const p = new Future().bind();
+      benchmark('Future   chain 100', () => void chain(p, 100), done);
+    });
+
+    it('AFuture  chain 100', function (done) {
+      const p = new AtomicFuture().bind();
+      benchmark('AFuture  chain 100', () => void chain(p, 100), done);
+    });
+
+    it('Promise  all 2', function (done) {
       const ps = [
         Promise.resolve(),
         Promise.resolve(),
@@ -107,15 +154,7 @@ describe('Benchmark:', function () {
       benchmark('Promise  all 2', done => void Promise.all(ps).then(done), done, { defer: true });
     });
 
-    it('all 2', function (done) {
-      const ps = [
-        AtomicPromise.resolve(),
-        AtomicPromise.resolve(),
-      ];
-      benchmark('APromise all 2', () => void AtomicPromise.all(ps).then(noop), done);
-    });
-
-    it('all 2', function (done) {
+    it('Bluebird all 2', function (done) {
       const ps = [
         Bluebird.resolve(),
         Bluebird.resolve(),
@@ -123,7 +162,15 @@ describe('Benchmark:', function () {
       benchmark('Bluebird all 2', done => void Bluebird.all(ps).then(done), done, { defer: true });
     });
 
-    it('all 3', function (done) {
+    it('APromise all 2', function (done) {
+      const ps = [
+        AtomicPromise.resolve(),
+        AtomicPromise.resolve(),
+      ];
+      benchmark('APromise all 2', () => void AtomicPromise.all(ps).then(noop), done);
+    });
+
+    it('Promise  all 3', function (done) {
       const ps = [
         Promise.resolve(),
         Promise.resolve(),
@@ -132,16 +179,7 @@ describe('Benchmark:', function () {
       benchmark('Promise  all 3', done => void Promise.all(ps).then(done), done, { defer: true });
     });
 
-    it('all 3', function (done) {
-      const ps = [
-        AtomicPromise.resolve(),
-        AtomicPromise.resolve(),
-        AtomicPromise.resolve(),
-      ];
-      benchmark('APromise all 3', () => void AtomicPromise.all(ps).then(noop), done);
-    });
-
-    it('all 3', function (done) {
+    it('Bluebird all 3', function (done) {
       const ps = [
         Bluebird.resolve(),
         Bluebird.resolve(),
@@ -150,7 +188,16 @@ describe('Benchmark:', function () {
       benchmark('Bluebird all 3', done => void Bluebird.all(ps).then(done), done, { defer: true });
     });
 
-    it('race 2', function (done) {
+    it('APromise all 3', function (done) {
+      const ps = [
+        AtomicPromise.resolve(),
+        AtomicPromise.resolve(),
+        AtomicPromise.resolve(),
+      ];
+      benchmark('APromise all 3', () => void AtomicPromise.all(ps).then(noop), done);
+    });
+
+    it('Promise  race 2', function (done) {
       const ps = [
         new Promise(() => 0),
         Promise.resolve(),
@@ -158,15 +205,7 @@ describe('Benchmark:', function () {
       benchmark('Promise  race 2', done => void Promise.race(ps).then(done), done, { defer: true });
     });
 
-    it('race 2', function (done) {
-      const ps = [
-        new AtomicPromise(() => 0),
-        AtomicPromise.resolve(),
-      ];
-      benchmark('APromise race 2', () => void AtomicPromise.race(ps).then(noop), done);
-    });
-
-    it('race 2', function (done) {
+    it('Bluebird race 2', function (done) {
       const ps = [
         new Bluebird(() => 0),
         Bluebird.resolve(),
@@ -174,7 +213,15 @@ describe('Benchmark:', function () {
       benchmark('Bluebird race 2', done => void Bluebird.race(ps).then(done), done, { defer: true });
     });
 
-    it('race 3', function (done) {
+    it('APromise race 2', function (done) {
+      const ps = [
+        new AtomicPromise(() => 0),
+        AtomicPromise.resolve(),
+      ];
+      benchmark('APromise race 2', () => void AtomicPromise.race(ps).then(noop), done);
+    });
+
+    it('Promise  race 3', function (done) {
       const ps = [
         new Promise(resolve => clock.then(resolve)),
         Promise.resolve(),
@@ -183,22 +230,22 @@ describe('Benchmark:', function () {
       benchmark('Promise  race 3', done => void Promise.race(ps).then(done), done, { defer: true });
     });
 
-    it('race 3', function (done) {
-      const ps = [
-        new AtomicPromise(resolve => clock.then(resolve)),
-        AtomicPromise.resolve(),
-        new AtomicPromise(() => 0),
-      ];
-      benchmark('APromise race 3', () => void AtomicPromise.race(ps).then(noop), done);
-    });
-
-    it('race 3', function (done) {
+    it('Bluebird race 3', function (done) {
       const ps = [
         new Bluebird(resolve => clock.then(resolve)),
         Bluebird.resolve(),
         new Bluebird(() => 0),
       ];
       benchmark('Bluebird race 3', done => void Bluebird.race(ps).then(done), done, { defer: true });
+    });
+
+    it('APromise race 3', function (done) {
+      const ps = [
+        new AtomicPromise(resolve => clock.then(resolve)),
+        AtomicPromise.resolve(),
+        new AtomicPromise(() => 0),
+      ];
+      benchmark('APromise race 3', () => void AtomicPromise.race(ps).then(noop), done);
     });
 
   });
