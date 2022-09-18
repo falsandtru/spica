5306128a0eae7dea4ddc46133186ef562760f69c
falsandtru <falsandtru@users.noreply.github.com>
2022-09-18 13:20:18 +0900

    Refactoring

--- src/timer.ts
+++ src/timer.ts
@@ -1,23 +1,49 @@
 import { setTimeout, clearTimeout } from './global';
+import { List } from './invlist';
 import { clock } from './clock';
 import { singleton, noop } from './function';
+
+interface Timer {
+  <T>(timeout: number, handler: () => T, unhandler?: (result: Awaited<T>) => void): () => void;
+  group(): GroupTimer;
+}
+interface GroupTimer {
+  <T>(timeout: number, handler: () => T, unhandler?: (result: Awaited<T>) => void): () => void;
+  clear(): void;
+}
 
 export const setTimer = template(false);
 export const setRepeatTimer = template(true);
 
-function template(repeat: boolean) {
-  return <a>(timeout: number, handler: () => a, unhandler?: (a: Awaited<a>) => void): () => void => {
-    let params: [Awaited<a>];
+function template(repeat: boolean): Timer;
+function template(repeat: boolean, cancellers: List<() => void>): GroupTimer;
+function template(repeat: boolean, cancellers?: List<() => void>): Timer | GroupTimer {
+  const timer = ((timeout, handler, unhandler?): () => void => {
+    let params: [Awaited<ReturnType<typeof handler>>];
     let id = setTimeout(async function loop() {
       params = [await handler()];
       if (!repeat) return;
       id = setTimeout(loop, timeout);
     }, timeout);
-    return singleton(() => {
+    const cancel = singleton(() => {
       clearTimeout(id);
+      node?.delete();
       params && unhandler?.(params[0]);
     });
-  };
+    const node = cancellers?.push(cancel);
+    return cancel;
+  }) as Timer & GroupTimer;
+  if (!cancellers) {
+    timer.group = () => template(repeat, new List());
+  }
+  else {
+    timer.clear = () => {
+      while (cancellers.length) {
+        cancellers.shift()!();
+      }
+    };
+  }
+  return timer;
 }
 
 export function captureTimers(test: (done: (err?: unknown) => void) => unknown): (done: (err?: unknown) => unknown) => void {
