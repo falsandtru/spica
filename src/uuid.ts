85494a5ba5e838fbb391d31946890549ae817046
falsandtru <falsandtru@users.noreply.github.com>
2022-10-09 04:16:59 +0900

    Refactoring

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,57 +1,38 @@
-// Version 4
+// UUID Version 4
 
-const format = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+const buffer = new Uint16Array(32 / 4 * 64);
+assert(buffer.length === 512);
+assert(buffer.length / (32 / 4) >> 0 === buffer.length / (32 / 4));
+const HEX = [...Array(16)].map((_, i) => i.toString(16)).join('');
+assert(HEX);
+let index = 0;
 
 export function uuid(): string {
-  let acc = '';
-  for (let i = 0; i < format.length; ++i) {
-    const c = format[i];
+  if (index === 0) {
+    crypto.getRandomValues(buffer);
+    index = buffer.length;
+  }
+  index -= 32 / 4;
+  return gen();
+}
+
+assert(eval('(function () {return this})()') === undefined);
+const gen = ((i, offset) => eval([
+  '() => {',
+  ...[...Array(32 / 4)].map((_, i) => `const buf${i} = buffer[index + ${i}];`),
+  'return',
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/./g, c => {
+    assert(i < 32);
+    assert(offset >= 0);
+    offset ||= 16;
     switch (c) {
       case 'x':
-        acc += HEX[rnd16()];
-        continue
+        return `+ HEX[buf${i++ >> 2} >> ${offset -= 4} & 15]`;
       case 'y':
-        acc += HEX[rnd16() & 0x03 | 0x08];
-        continue
+        return `+ HEX[buf${i++ >> 2} >> ${offset -= 4} & 0x03 | 0x08]`;
       default:
-        acc += c;
-        continue
+        return `+ '${c}'`;
     }
-  }
-  return acc;
-}
-
-const HEX = [...Array(16)].map((_, i) => i.toString(16)).join('');
-
-const buffer = new Uint16Array(512);
-assert(buffer.length % 4 === 0);
-const digit = 16;
-const mask = digit - 1;
-let index = buffer.length;
-let rnd = 2 ** digit;
-let offset = digit;
-
-function rnd16(): number {
-  if (rnd === 2 ** digit) {
-    assert(index === buffer.length);
-    crypto.getRandomValues(buffer);
-    index = 0;
-    rnd = buffer[index];
-    assert(offset === digit);
-  }
-  if (offset === 4) {
-    offset = digit;
-    assert((rnd & mask) >= 0);
-    assert((rnd & mask) < 16);
-    const r = rnd & mask;
-    rnd = buffer[++index] ?? 2 ** digit;
-    return r;
-  }
-  else {
-    assert(offset > 4);
-    offset -= 4;
-    assert((rnd >> offset & mask) >= 0);
-    assert((rnd >> offset & mask) < 16);
-    return rnd >> offset & mask;
-  }
-}
+  }).slice(1),
+  '}',
+].join('')))(0, 0);
