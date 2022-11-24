88e84902c3359aeb6d84b64920bc326106e31a48
falsandtru <falsandtru@users.noreply.github.com>
2022-11-24 14:49:20 +0900

    Refactoring

--- src/cache.test.ts
+++ src/cache.test.ts
@@ -392,7 +392,7 @@ describe('Unit: lib/cache', () => {
       console.debug('DWC hit ratio', dwchit * 100 / trials);
       console.debug('DWC ratio', dwc['ratio']! / 10 | 0, dwc['indexes'].LFU.length * 100 / dwc.length | 0);
       console.debug('DWC / LRU hit ratio rate', `${dwchit / lruhit * 100 | 0}%`);
-      assert(dwchit / lruhit * 100 >>> 0 === 195);
+      assert(dwchit / lruhit * 100 >>> 0 === 194);
     });
 
     it('ratio uneven 100 transitive distribution', function () {
