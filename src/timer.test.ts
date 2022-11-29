bb55fe1e97f22309f3da0a3997c2a5f9bba379fd
falsandtru <falsandtru@users.noreply.github.com>
2022-11-29 10:17:55 +0900

    Refactoring

---
+++ src/timer.test.ts
@@ -0,0 +1,11 @@
+import { setTimer } from './timer';
+
+describe('Unit: lib/timer', () => {
+  describe('setTimet', () => {
+    it('', (done) => {
+      setTimer(100, done)
+    });
+
+  });
+
+});
