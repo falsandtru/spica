d480effb1049b9fae31188b61c3adf6c5d54cc07
falsandtru <falsandtru@users.noreply.github.com>
2022-12-01 14:01:45 +0900

    Refactoring

--- benchmark/array.ts
+++ benchmark/array.ts
@@ -3,6 +3,16 @@ import { benchmark } from './benchmark';
 describe('Benchmark:', function () {
   describe('unshift', function () {
     for (const length of [1, 1e1, 1e2, 1e3]) {
+      it(`for ${length.toLocaleString('en')}`, function (done) {
+        const as = Array(length).fill(1);
+        benchmark(`Array unshift for    ${length.toLocaleString('en')}`, () => {
+          const acc = [];
+          for (let i = as.length; i-- !== 0;) {
+            acc.unshift(as[i]);
+          }
+        }, done);
+      });
+
       it(`for-of ${length.toLocaleString('en')}`, function (done) {
         const as = Array(length).fill(1);
         benchmark(`Array unshift for-of ${length.toLocaleString('en')}`, () => {
@@ -25,6 +35,16 @@ describe('Benchmark:', function () {
 
   describe('push', function () {
     for (const length of [1, 1e1, 1e2, 1e3]) {
+      it(`for ${length.toLocaleString('en')}`, function (done) {
+        const as = Array(length).fill(1);
+        benchmark(`Array push for    ${length.toLocaleString('en')}`, () => {
+          const acc = [];
+          for (let i = 0; i < as.length; ++i) {
+            acc.push(as[i]);
+          }
+        }, done);
+      });
+
       it(`for-of ${length.toLocaleString('en')}`, function (done) {
         const as = Array(length).fill(1);
         benchmark(`Array push for-of ${length.toLocaleString('en')}`, () => {
