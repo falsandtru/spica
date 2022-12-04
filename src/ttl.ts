265c0a2d37b91257fb343542daffb1e17ee2f61a
falsandtru <falsandtru@users.noreply.github.com>
2022-12-04 16:30:38 +0900

    Refactoring

--- src/ttl.ts
+++ src/ttl.ts
@@ -2,6 +2,7 @@ import { max, floor } from './alias';
 import { List } from './list';
 import { now } from './chrono';
 
+const DIGIT1 = 0;
 const DIGIT2 = 32 / 4;
 assert(DIGIT2 === 8);
 const DIGIT3 = DIGIT2 * 2;
@@ -36,21 +37,9 @@ export class TTL<T = undefined> {
     assert(seg === seg >>> 0);
     return seg / OVERFLOW >>> 0;
   }
-  private static index4(seg: number): number {
+  private static index(seg: number, digit: number): number {
     assert(seg === seg >>> 0);
-    return seg >>> DIGIT4 & MASK;
-  }
-  private static index3(seg: number): number {
-    assert(seg === seg >>> 0);
-    return seg >>> DIGIT3 & MASK;
-  }
-  private static index2(seg: number): number {
-    assert(seg === seg >>> 0);
-    return seg >>> DIGIT2 & MASK;
-  }
-  private static index1(seg: number): number {
-    assert(seg === seg >>> 0);
-    return seg & MASK;
+    return seg >>> digit & MASK;
   }
   constructor(
     private resolution = 16,
@@ -72,17 +61,8 @@ export class TTL<T = undefined> {
   private get overflow(): number {
     return this.earliest.segment / OVERFLOW >>> 0;
   }
-  private get index4(): number {
-    return this.earliest.segment >>> DIGIT4 & MASK;
-  }
-  private get index3(): number {
-    return this.earliest.segment >>> DIGIT3 & MASK;
-  }
-  private get index2(): number {
-    return this.earliest.segment >>> DIGIT2 & MASK;
-  }
-  private get index1(): number {
-    return this.earliest.segment & MASK;
+  private index(digit: number): number {
+    return this.earliest.segment >>> digit & MASK;
   }
   private seek(): void {
     assert(this.earliest.length === 0);
@@ -91,19 +71,19 @@ export class TTL<T = undefined> {
     for (; i5 < this.wheel.length; ++i5) {
       const l4 = this.wheel[i5];
       if (l4 === undefined) continue;
-      let i4 = cont ? this.index4 : 0;
+      let i4 = cont ? this.index(DIGIT4) : 0;
       for (; i4 < l4.length; ++i4) {
         const l3 = l4[i4];
         if (l3 === undefined) continue;
-        let i3 = cont ? this.index3 : 0;
+        let i3 = cont ? this.index(DIGIT3) : 0;
         for (; i3 < l3.length; ++i3) {
           const l2 = l3[i3];
           if (l2 === undefined) continue;
-          let i2 = cont ? this.index2 : 0;
+          let i2 = cont ? this.index(DIGIT2) : 0;
           for (; i2 < l2.length; ++i2) {
             const l1 = l2[i2];
             if (l1 === undefined) continue;
-            let i1 = cont ? this.index1 : 0;
+            let i1 = cont ? this.index(DIGIT1) : 0;
             for (; i1 < l1.length; ++i1) {
               const queue = l1[i1];
               if (queue === undefined) continue;
@@ -132,10 +112,10 @@ export class TTL<T = undefined> {
   }
   private queue(seg: number): Queue<T> {
     const l4 = this.wheel[TTL.overflow(seg)] ??= [];
-    const l3 = l4[TTL.index4(seg)] ??= [];
-    const l2 = l3[TTL.index3(seg)] ??= [];
-    const l1 = l2[TTL.index2(seg)] ??= [];
-    const qu = l1[TTL.index1(seg)] ??= new Queue(seg);
+    const l3 = l4[TTL.index(seg, DIGIT4)] ??= [];
+    const l2 = l3[TTL.index(seg, DIGIT3)] ??= [];
+    const l1 = l2[TTL.index(seg, DIGIT2)] ??= [];
+    const qu = l1[TTL.index(seg, DIGIT1)] ??= new Queue(seg);
     return qu;
   }
   public peek(): TTL.Node<T> | undefined {
