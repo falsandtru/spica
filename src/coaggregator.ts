30da45055af801b14303e5c12557c4ff87208c1f
falsandtru <falsandtru@users.noreply.github.com>
2023-06-22 22:11:08 +0900

    Refactoring

--- src/coaggregator.ts
+++ src/coaggregator.ts
@@ -1,28 +1,32 @@
+import { isArray } from './alias';
 import { Coroutine, CoroutineOptions } from './coroutine';
 import { AtomicPromise, never } from './promise';
 import { select } from './select';
 
 export class Coaggregator<T = unknown, R = T, S = unknown> extends Coroutine<T, R, S> {
   constructor(
-    coroutines: readonly Coroutine<T, R, S>[],
+    coroutines: Iterable<Coroutine<T, R, S>>,
     reducer: (results: T[]) => T = results => results[0],
     opts?: CoroutineOptions,
   ) {
     super(async function* () {
+      const cs = isArray(coroutines)
+        ? coroutines
+        : [...coroutines];
       this.then(
         result => {
-          for (const co of coroutines) {
+          for (const co of cs) {
             co[Coroutine.exit](result);
           }
         },
         reason => {
           const rejection = AtomicPromise.reject(reason);
-          for (const co of coroutines) {
+          for (const co of cs) {
             co[Coroutine.exit](rejection);
           }
         });
-      const results: T[] = Array(coroutines.length);
-      for await (const { name, result } of select(coroutines)) {
+      const results: T[] = [];
+      for await (const { name, result } of select(cs)) {
         assert(Number.isSafeInteger(+name));
         if (result.done) {
           results[name] = result.value;
@@ -32,7 +36,7 @@ export class Coaggregator<T = unknown, R = T, S = unknown> extends Coroutine<T, 
         }
       }
       assert(Object.keys(results).length === results.length);
-      assert(results.length === coroutines.length);
+      assert(results.length === cs.length);
       results.length === 0
         ? this[Coroutine.terminate](new Error(`Spica: Coaggregator: No result.`))
         : this[Coroutine.exit](reducer(results));
--- src/copropagator.ts
+++ src/copropagator.ts
@@ -2,29 +2,29 @@ import { isArray } from './alias';
 import { Coroutine, CoroutineOptions } from './coroutine';
 import { AtomicPromise, never } from './promise';
 
-// Must support living iterables.
-
 export class Copropagator<T = unknown, R = T, S = unknown> extends Coroutine<T, R, S> {
   constructor(
-    coroutines: Iterable<Coroutine<T, R, unknown>>,
+    coroutines: Iterable<Coroutine<T, unknown, unknown>>,
     reducer: (results: T[]) => T = results => results[0],
     opts?: CoroutineOptions,
   ) {
-    assert(new Set(coroutines).size === [...coroutines].length);
+    const cs = isArray(coroutines)
+      ? coroutines
+      : [...coroutines];
     super(async function* () {
       this.then(
         result => {
-          for (const co of coroutines) {
+          for (const co of cs) {
             co[Coroutine.exit](result);
           }
         },
         reason => {
           const rejection = AtomicPromise.reject(reason);
-          for (const co of coroutines) {
+          for (const co of cs) {
             co[Coroutine.exit](rejection);
           }
         });
-      all(coroutines).then(
+      AtomicPromise.all(cs).then(
         results =>
           results.length === 0
             ? void this[Coroutine.terminate](new Error(`Spica: Copropagator: No result.`))
@@ -34,24 +34,4 @@ export class Copropagator<T = unknown, R = T, S = unknown> extends Coroutine<T, 
       return never;
     }, { delay: false, ...opts });
   }
-}
-
-function all<T>(sources: Iterable<PromiseLike<T>>, memory?: Map<PromiseLike<T>, T>): AtomicPromise<T[]> {
-  const before = isArray(sources)
-    ? sources
-    : [...sources];
-  return AtomicPromise.all(before).then(values => {
-    const after = isArray(sources)
-      ? sources
-      : [...sources];
-    const same = after.length === before.length && after.every((_, i) => after[i] === before[i]);
-    if (!memory && same) return values;
-    memory ??= new Map();
-    for (let i = 0; i < values.length; ++i) {
-      memory.set(before[i], values[i]);
-    }
-    return same
-      ? [...memory.values()]
-      : all(after, memory);
-  });
 }
