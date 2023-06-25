42a5228d2a33b10a0806cef9432e1c98b23cd999
falsandtru <falsandtru@users.noreply.github.com>
2023-06-26 02:22:01 +0900

    Refactoring

--- src/counter.ts
+++ src/counter.ts
@@ -1,5 +1,3 @@
-import { max } from './alias';
-
 const dict = [...Array(36)].map((_, i) => i.toString(36)).join('');
 assert(dict.length === 36);
 assert(dict[0] === '0');
@@ -8,17 +6,26 @@ assert(dict.at(-1) === 'z');
 export function counter(radix: number = 10, pad: string = ''): () => string {
   assert(radix <= 36);
   let cnt0 = 0;
+  if (radix === 10) return format(pad, () => `${++cnt0}`);
   let cnt1 = 0;
   let str1 = '';
   let cnt2 = 0;
   let str2 = '';
-  return () => {
+  return format(pad, () => {
     const digit0 = dict[cnt0 = ++cnt0 % radix];
     const digit1 = cnt0 ? str1 : str1 = dict[cnt1 = ++cnt1 % radix];
     const digitN = cnt0 | cnt1 ? str2 : str2 = (++cnt2).toString(radix);
-    const count = `${digitN}${digit1}${digit0}`;
-    return pad
-      ? (pad + count).slice(-max(pad.length, count.length))
-      : count;
-  };
+    return `${digitN}${digit1}${digit0}`;
+  });
+}
+
+function format(pad: string, counter: () => string): () => string {
+  return pad === ''
+    ? counter
+    : () => {
+        const count = counter();
+        return pad.length > count.length
+          ? `${pad.slice(0, pad.length - count.length)}${count}`
+          : count;
+      };
 }
